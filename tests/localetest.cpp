2e4841b3775995c7cf09d73a4d871f92e9aa6bcf
jansorg <jansorg@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2000-12-27 00:49:29 +0000

    Fixed a small problem with "Usage" print out.
    git-svn-id: https://crosswire.org/svn/sword/trunk@234 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/localetest.cpp
+++ tests/localetest.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
-		fprintf(stderr, "usage: %s localetest <locale_name> <text>\n", *argv);
+		fprintf(stderr, "usage: %s <locale_name> <text>\n", *argv);
 		exit(-1);
 	}
 
