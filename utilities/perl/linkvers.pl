295adafb3feef36015402edfffd9e430408b52ba
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2001-09-03 19:00:52 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@426 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ utilities/perl/linkvers.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+$vplfile = $ARGV[0];
+
+if ($vplfile eq "") {
+    die("linkvers.pl Syntax:\n./linkvers.pl <vpl file> [1 - checking mode on].\nMust be run AFTER vpl2mod is completed.\ndied");
+}
+
+$check = $ARGV[1];
+
+open(INF,$vplfile) or die;
+while (<INF>) {
+    $line = $_;
+
+    $line =~ /([\w ]+:[\d\-]+)\s+(.*)/;
+    $vref = $1;
+
+    if ($vref =~ /\-/) {
+	$vref =~ /(.*:)(\d+)\-(\d+)/;
+	$ch = $1;
+	$fv = $2;
+	$lv = $3;
+	if ($fv + 1 == $lv) {
+	    $sv = $lv;
+	}
+	else {
+	    $sv = $fv + 1;
+	    $sv .= "-" . $lv;
+	}
+	$first = $ch . $fv;
+	$last = $ch . $sv;
+
+	if ($check ne "") {
+	    print "$first\t\t$last";
+	} else {
+	    `addvs -l ./ \"$first\" \"$last\"`;
+	}
+    }
+}
+close(INF);
+
+
+
+
