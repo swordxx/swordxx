6bd123e5ca73d6c6eced11287ee6a08994bd402c
tbiggs <tbiggs@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2001-09-03 16:28:37 +0000

    add files gbfhtmlhref.h thmlhtmlhref.h <tbiggs@users.sourceforge.net>
    git-svn-id: https://crosswire.org/svn/sword/trunk@423 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ include/gbfhtmlhref.h
@@ -0,0 +1,33 @@
+/*-*************************************************************************
+                          gbfhtmlhref.h  -  description
+                             -------------------
+    begin                    : 2001-09-03
+    copyright            : 2001 by CrossWire Bible Society
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef GBFHTMLHREF_H
+#define GBFHTMLHREF_H
+
+#include <swbasicfilter.h>
+
+#include <defs.h>
+
+/** this filter converts GBF  text to HTML text with hrefs
+ */
+class SWDLLEXPORT GBFHTMLHREF : public SWBasicFilter {
+protected:
+	virtual bool handleToken(char **buf, const char *token);
+public:
+  GBFHTMLHREF ();
+};
+
+#endif
