5dae3ed109cbf0150fc9343da028bac22611cfd0
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2001-10-24 19:47:23 +0000

    added encoding & markup constructor args
    git-svn-id: https://crosswire.org/svn/sword/trunk@486 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/modules/comments/swcom.cpp
+++ src/modules/comments/swcom.cpp
@@ -14,7 +14,7 @@
  *	idisp	 - Display object to use for displaying
  */
 
-SWCom::SWCom(const char *imodname, const char *imoddesc, SWDisplay *idisp, bool unicode, char dir): SWModule(imodname, imoddesc, idisp, "Commentaries", unicode, dir)
+SWCom::SWCom(const char *imodname, const char *imoddesc, SWDisplay *idisp, char enc, char dir, char mark): SWModule(imodname, imoddesc, idisp, "Commentaries", enc, dir, mark)
 {
 	delete key;
 	key = CreateKey();
