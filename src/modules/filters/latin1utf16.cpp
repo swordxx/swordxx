9ddc84f9c226b5ea6b07aad8177f1b73640e8307
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2001-11-01 16:49:15 +0000

    adjusted to handle Windows Codepage 1252 instead of ISO 8859-1 (CP1252 is a superset of ISO 8859-1, so this won't cause any problems aside from inaccurate naming of the class)
    git-svn-id: https://crosswire.org/svn/sword/trunk@504 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/modules/filters/latin1utf16.cpp
+++ src/modules/filters/latin1utf16.cpp
@@ -29,7 +29,91 @@ char Latin1UTF16::ProcessText(char *text, int maxlen, const SWKey *key)
     // -------------------------------
     
     for (to = (unsigned short*)text; *from; from++) {
-      *to++ = (unsigned short)*from;
+        switch (*from) {
+	case 0x80: // '€'
+		*to++ = 0x20AC;
+		break;
+	case 0x82: // '‚'
+		*to++ = 0x201A;
+		break;
+	case 0x83: // 'ƒ'
+		*to++ = 0x0192;
+		break;
+	case 0x84: // '„'
+		*to++ = 0x201E;
+		break;
+	case 0x85: // '…'
+		*to++ = 0x2026;
+		break;
+	case 0x86: // '†'
+		*to++ = 0x2020;
+		break;
+	case 0x87: // '‡'
+		*to++ = 0x2021;
+		break;
+	case 0x88: // 'ˆ'
+		*to++ = 0x02C6;
+		break;
+	case 0x89: // '‰'
+		*to++ = 0x2030;
+		break;
+	case 0x8A: // 'Š'
+		*to++ = 0x0160;
+		break;
+	case 0x8B: // '‹'
+		*to++ = 0x2039;
+		break;
+	case 0x8C: // 'Œ'
+		*to++ = 0x0152;
+		break;
+	case 0x8E: // 'Ž'
+		*to++ = 0x017D;
+		break;
+	case 0x91: // '‘'
+		*to++ = 0x2018;
+		break;
+	case 0x92: // '’'
+		*to++ = 0x2019;
+		break;
+	case 0x93: // '“'
+		*to++ = 0x201C;
+		break;
+	case 0x94: // '”'
+		*to++ = 0x201D;
+		break;
+	case 0x95: // '•'
+		*to++ = 0x2022;
+		break;
+	case 0x96: // '–'
+		*to++ = 0x2013;
+		break;
+	case 0x97: // '—'
+		*to++ = 0x2014;
+		break;
+	case 0x98: // '˜'
+		*to++ = 0x02DC;
+		break;
+	case 0x99: // '™'
+		*to++ = 0x2122;
+		break;
+	case 0x9A: // 'š'
+		*to++ = 0x0161;
+		break;
+	case 0x9B: // '›'
+		*to++ = 0x203A;
+		break;
+	case 0x9C: // 'œ'
+		*to++ = 0x0153;
+		break;
+	case 0x9E: // 'ž'
+		*to++ = 0x017E;
+		break;
+	case 0x9F: // 'Ÿ'
+		*to++ = 0x0178;
+		break;
+        default:
+               *to++ = (unsigned short)*from;
+        }
     }
     *to = 0;
     return 0;
--- src/modules/filters/latin1utf8.cpp
+++ src/modules/filters/latin1utf8.cpp
@@ -30,13 +30,143 @@ char Latin1UTF8::ProcessText(char *text, int maxlen, const SWKey *key)
 	else	
 	  from = (unsigned char*)text;							// -------------------------------
 
+
+
 	for (to = (unsigned char*)text; *from; from++) {
 	  if (*from < 0x80) {
 	    *to++ = *from;
 	  }
 	  else if (*from < 0xc0) {
-	    *to++ = 0xC2;
-	    *to++ = *from;
+                switch(*from) {
+        	case 0x80: // '€'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x82; // '‚'
+        		*to++ = 0xac; // '¬'
+	        	break;
+        	case 0x82: // '‚'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0x9a; // 'š'
+	        	break;
+        	case 0x83: // 'ƒ'
+	        	*to++ = 0xc6; // 'Æ'
+		        *to++ = 0x92; // '’'
+        		break;
+	        case 0x84: // '„'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0x9e; // 'ž'
+		        break;
+        	case 0x85: // '…'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0xa6; // '¦'
+	        	break;
+        	case 0x86: // '†'
+        		*to++ = 0xe2; // 'â'
+	        	*to++ = 0x80; // '€'
+		        *to++ = 0xa0; // ' '
+        		break;
+	        case 0x87: // '‡'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0xa1; // '¡'
+		        break;
+        	case 0x88: // 'ˆ'
+	        	*to++ = 0xcb; // 'Ë'
+		        *to++ = 0x86; // '†'
+        		break;
+	        case 0x89: // '‰'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0xb0; // '°'
+		        break;
+        	case 0x8A: // 'Š'
+	        	*to++ = 0xc5; // 'Å'
+		        *to++ = 0xa0; // ' '
+        		break;
+	        case 0x8B: // '‹'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0xb9; // '¹'
+		        break;
+        	case 0x8C: // 'Œ'
+	        	*to++ = 0xc5; // 'Å'
+		        *to++ = 0x92; // '’'
+        		break;
+	        case 0x8E: // 'Ž'
+		        *to++ = 0xc5; // 'Å'
+        		*to++ = 0xbd; // '½'
+	        	break;
+        	case 0x91: // '‘'
+        		*to++ = 0xe2; // 'â'
+	        	*to++ = 0x80; // '€'
+		        *to++ = 0x98; // '˜'
+        		break;
+	        case 0x92: // '’'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0x99; // '™'
+		        break;
+        	case 0x93: // '“'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0x9c; // 'œ'
+	        	break;
+        	case 0x94: // '”'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0x9d; // ''
+	        	break;
+        	case 0x95: // '•'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0xa2; // '¢'
+	        	break;
+        	case 0x96: // '–'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0x93; // '“'
+	        	break;
+        	case 0x97: // '—'
+	        	*to++ = 0xe2; // 'â'
+		        *to++ = 0x80; // '€'
+        		*to++ = 0x94; // '”'
+	        	break;
+        	case 0x98: // '˜'
+	        	*to++ = 0xcb; // 'Ë'
+		        *to++ = 0x9c; // 'œ'
+        		break;
+	        case 0x99: // '™'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x84; // '„'
+	        	*to++ = 0xa2; // '¢'
+		        break;
+        	case 0x9A: // 'š'
+	        	*to++ = 0xc5; // 'Å'
+		        *to++ = 0xa1; // '¡'
+        		break;
+	        case 0x9B: // '›'
+		        *to++ = 0xe2; // 'â'
+        		*to++ = 0x80; // '€'
+	        	*to++ = 0xba; // 'º'
+		        break;
+        	case 0x9C: // 'œ'
+	        	*to++ = 0xc5; // 'Å'
+		        *to++ = 0x93; // '“'
+        		break;
+	        case 0x9E: // 'ž'
+		        *to++ = 0xc5; // 'Å'
+        		*to++ = 0xbe; // '¾'
+	        	break;
+        	case 0x9F: // 'Ÿ'
+	        	*to++ = 0xc5; // 'Å'
+		        *to++ = 0xb8; // '¸'
+        		break;
+                default:
+                        *to++ = 0xC2;
+                        *to++ = *from;
+                }
 	  }
 	  else {
 	    *to++ = 0xC3;
