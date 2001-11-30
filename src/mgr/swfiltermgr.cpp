d20ae3adbe844b565be138ee8e8f78558b62661a
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2001-11-30 12:04:34 +0000

    moved constructor methods to .cpp
    git-svn-id: https://crosswire.org/svn/sword/trunk@568 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/mgr/swfiltermgr.cpp
+++ src/mgr/swfiltermgr.cpp
@@ -2,7 +2,7 @@
  *  swfiltermgr.cpp   - definition of class SWFilterMgr used as an interface to
  *				manage filters on a module
  *
- * $Id: swfiltermgr.cpp,v 1.1 2001/11/30 11:16:15 scribe Exp $
+ * $Id: swfiltermgr.cpp,v 1.2 2001/11/30 12:04:34 scribe Exp $
  *
  * Copyright 1998 CrossWire Bible Society (http://www.crosswire.org)
  *	CrossWire Bible Society
@@ -23,11 +23,68 @@
 #include <swfiltermgr.h>
 
 
-void SWFilterMgr::setParentMgr(SWMgr *parentMgr) {
-  this->parentMgr = parentMgr;
+SWFilterMgr::SWFilterMgr() {
 }
 
+
+SWFilterMgr::~SWFilterMgr() {
+}
+
+
+void SWFilterMgr::setParentMgr(SWMgr *parentMgr) {
+	this->parentMgr = parentMgr;
+}
+
+
 SWMgr *SWFilterMgr::getParentMgr() {
-  return parentMgr;
+	return parentMgr;
+}
+
+
+void SWFilterMgr::AddGlobalOptions(SWModule * module, ConfigEntMap & section, ConfigEntMap::iterator start, ConfigEntMap::iterator end) {
+}
+
+
+void SWFilterMgr::AddLocalOptions(SWModule * module, ConfigEntMap & section, ConfigEntMap::iterator start, ConfigEntMap::iterator end) {
+}
+
+
+/**
+* Adds the encoding filters which are defined in "section" to the SWModule object "module".
+* @param module To this module the encoding filter(s) are added
+* @param section We use this section to get a list of filters we should apply to the module
+*/
+
+void SWFilterMgr::AddEncodingFilters(SWModule * module, ConfigEntMap & section) {
+}
+
+
+/**
+* Adds the render filters which are defined in "section" to the SWModule object "module".
+* @param module To this module the render filter(s) are added
+* @param section We use this section to get a list of filters we should apply to the module
+*/
+
+void SWFilterMgr::AddRenderFilters(SWModule * module, ConfigEntMap & section) {
+}
+
+
+/**
+* Adds the strip filters which are defined in "section" to the SWModule object "module".
+* @param module To this module the strip filter(s) are added
+* @param section We use this section to get a list of filters we should apply to the module
+*/
+
+void SWFilterMgr::AddStripFilters(SWModule * module, ConfigEntMap & section) {
+}
+
+
+/**
+* Adds the raw filters which are defined in "section" to the SWModule object "module".
+* @param module To this module the raw filter(s) are added
+* @param section We use this section to get a list of filters we should apply to the module
+*/
+
+void SWFilterMgr::AddRawFilters(SWModule * module, ConfigEntMap & section) {
 }
 
