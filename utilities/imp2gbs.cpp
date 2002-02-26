01e186c3aabdbfd8d33e20afa56c42b29fb6e9bf
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2002-02-26 00:28:56 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@667 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ utilities/imp2gbs.cpp
@@ -0,0 +1,153 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#ifndef __GNUC__
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <entriesblk.h>
+#include <iostream.h>
+#include <string>
+#include <stdio.h>
+#include <treekeyidx.h>
+#include <rawgenbook.h>
+
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif
+
+void printTree(TreeKeyIdx treeKey, TreeKeyIdx *target = 0, int level = 1) {
+  if (!target)
+    target = &treeKey;
+  
+  unsigned long currentOffset = target->getOffset();
+  cout << ((currentOffset == treeKey.getOffset()) ? "==>" : "");
+  for (int i = 0; i < level; i++) cout << "\t";
+  cout << treeKey.getLocalName() << "/\n";
+  if (treeKey.firstChild()) {
+    printTree(treeKey, target, level+1);
+    treeKey.parent();
+  }
+  if (treeKey.nextSibling())
+    printTree(treeKey, target, level);
+}
+
+void setkey (TreeKeyIdx * treeKey, char* keybuffer) {
+  char* tok = strtok(keybuffer, "/");
+  while (tok) {
+    bool foundkey = false;
+    if (treeKey->hasChildren()) {
+      treeKey->firstChild();
+      if (!strcmp(treeKey->getLocalName(), tok)) {
+	foundkey = true;
+      } else {
+	while (treeKey->nextSibling()) {
+	  if (treeKey->getLocalName()) {
+	    if (!strcmp(treeKey->getLocalName(), tok)) {
+	      foundkey = true;
+	    }
+	  }
+	}
+      }
+      if (!foundkey) {
+	treeKey->append();
+	treeKey->setLocalName(tok);
+	treeKey->save();	    
+      }
+    }
+    else {
+      treeKey->appendChild();
+      treeKey->setLocalName(tok);
+      treeKey->save();
+    }
+    
+    //DEBUG      cout << treeKey->getLocalName() << " : " << tok << endl;
+    
+    tok = strtok(NULL, "/");
+    
+  }
+}
+
+int readline(FILE* infile, char* linebuffer) {
+  char c;
+  char* lbPtr = linebuffer;
+  while ((c = fgetc(infile)) != EOF) {
+    *lbPtr++ = c;
+    if (c == 10) {
+      *lbPtr = 0;
+      return (strlen(linebuffer));
+    }
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  
+  const char * helptext ="imp2gbs 1.0 General Book module creation tool for the SWORD Project\n  usage:\n   %s <filename> [modname]\n";
+  
+  signed long i = 0;
+  char keybuffer[2048];
+  char entbuffer[1048576];
+  char linebuffer[1048576];
+  char modname[16];
+  
+  if (argc > 2) {
+    strcpy (modname, argv[2]);
+  }
+  else if (argc > 1) {
+    for (i = 0; (i < 16) && (argv[1][i]) && (argv[1][i] != '.'); i++) {
+      modname[i] = argv[1][i];
+    }
+    modname[i] = 0;
+  }
+  else {
+    fprintf(stderr, helptext, argv[0]);
+    exit(-1);
+  }
+  
+  FILE *infile;
+  infile = fopen(argv[1], "r");
+  
+  TreeKeyIdx * treeKey;
+  RawGenBook *book;
+  
+  // Do some initialization stuff
+  TreeKeyIdx::create(modname);
+  treeKey = new TreeKeyIdx(modname);
+  RawGenBook::createModule(modname);
+  delete treeKey;
+  book = new RawGenBook(modname);
+  //DEBUG  TreeKeyIdx root = *((TreeKeyIdx *)((SWKey *)(*book)));
+  treeKey = ((TreeKeyIdx *)((SWKey *)(*book)));
+  
+  while (readline(infile, linebuffer)) {
+    if (!strncmp(linebuffer, "$$$", 3)) {
+      if (strlen(keybuffer) && strlen(entbuffer)) {
+	cout << keybuffer << endl;
+	treeKey->root();
+	setkey(treeKey, keybuffer);
+	book->setentry(entbuffer, strlen(entbuffer));
+      }
+      linebuffer[strlen(linebuffer) - 1] = 0;
+      strcpy (keybuffer, linebuffer + 3);
+      *entbuffer = 0;
+    }
+    else {
+      strcat (entbuffer, linebuffer);
+    }
+  }
+  
+  //DEBUG  printTree(root, treeKey);
+  
+  delete treeKey;
+  delete book;
+  return 0;
+}
+
+
+
