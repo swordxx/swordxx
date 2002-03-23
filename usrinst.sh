3d1c3a6d0a9df6b22ed7618990077ef1f176c500
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2002-03-23 03:33:11 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@743 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- usrinst.sh
+++ usrinst.sh
@@ -3,3 +3,21 @@
 ./configure --prefix=/usr \
 	--enable-debug --disable-shared \
 	--without-conf --sysconfdir=/etc
+
+echo ""
+echo ""
+echo ""
+echo "Configured to NOT write a global /etc/sword.conf on 'make install'."
+echo "If this is the first time you've installed sword, be sure to run"
+echo "'make install_config' if you would like a basic configuration installed"
+echo ""
+echo "Now you make type: "
+echo ""
+echo "make"
+echo "su"
+echo "make install"
+echo "(and optionally)"
+echo "make install_config"
+echo ""
+echo ""
+echo ""
