c399d5e64e70c40d6ec8ff4d100874e6ff5db9f3
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2002-06-12 00:35:28 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@792 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/introtest.cpp
+++ tests/introtest.cpp
@@ -1,10 +1,92 @@
 #include <swmgr.h>
 #include <iostream.h>
 #include <versekey.h>
+#include <rawtext.h>
 
 int main(int argc, char **argv) {
 	SWMgr mymgr;
 
+	RawText::createModule(".");
+	RawText mod(".");
+
+	VerseKey vk;
+	vk.Headings(1);
+	vk.AutoNormalize(0);
+	vk.Persist(1);
+	mod.SetKey(vk);
+
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(0);
+	vk.Testament(0);
+
+	mod << "Module heading text";
+
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(0);
+	vk.Testament(1);
+
+	mod << "OT heading text";
+
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(1);
+	vk.Testament(1);
+
+	mod << "Gen heading text";
+
+	vk.Verse(0);
+	vk.Chapter(1);
+	vk.Book(1);
+	vk.Testament(1);
+
+	mod << "Gen 1 heading text";
+
+	vk.Verse(1);
+	vk.Chapter(1);
+	vk.Book(1);
+	vk.Testament(1);
+
+	mod << "Gen 1:1 text";
+
+	
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(0);
+	vk.Testament(0);
+
+	cout << "Module heading text ?= " << (const char*)mod << endl;
+
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(0);
+	vk.Testament(1);
+
+	cout << "OT heading text ?= " << (const char*)mod << endl;
+
+	vk.Verse(0);
+	vk.Chapter(0);
+	vk.Book(1);
+	vk.Testament(1);
+
+	cout << "Gen heading text ?= " << (const char*)mod << endl;
+
+	vk.Verse(0);
+	vk.Chapter(1);
+	vk.Book(1);
+	vk.Testament(1);
+
+	cout << "Gen 1 heading text ?= " << (const char*)mod << endl;
+
+	vk.Verse(1);
+	vk.Chapter(1);
+	vk.Book(1);
+	vk.Testament(1);
+
+	cout << "Gen 1:1 text ?= " << (const char*)mod << endl;
+
+	  /* old introtest
 	SWModule *mhc = mymgr.Modules["MHC"];
 
 	if (mhc) {
@@ -18,5 +100,8 @@ int main(int argc, char **argv) {
 		cout << (const char *) mhc->Key() << ":\n";
 		cout << (const char *) *mhc << "\n";
 	}
+	  */
 	return 0;
 }
+
+
