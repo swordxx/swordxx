366d20bf28b7180d4cc10a80ae9b777acdb5d492
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2002-10-22 01:28:49 +0000

    no message
    git-svn-id: https://crosswire.org/svn/sword/trunk@971 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- include/swversion.h
+++ include/swversion.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *  swversion.h   - definition of class SWVersion used to compare version info
  *
- * $Id: swversion.h,v 1.8 2002/10/08 02:03:31 scribe Exp $
+ * $Id: swversion.h,v 1.9 2002/10/22 01:28:49 scribe Exp $
  *
  * Copyright 1998 CrossWire Bible Society (http://www.crosswire.org)
  *	CrossWire Bible Society
@@ -20,7 +20,7 @@
  */
 
 #ifndef SWORDVER
-#define SWORDVER "1.5.4a"
+#define SWORDVER "1.5.5"
 #endif
 
 #ifndef SWVERSION_H
