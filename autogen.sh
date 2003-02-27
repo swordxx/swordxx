08a7cb019676eef15a9be3c04dd51f28f4f5bce5
mgruner <mgruner@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2003-02-27 21:11:37 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@1102 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- autogen.sh
+++ autogen.sh
@@ -2,63 +2,14 @@
 ACLOCAL="$AUTODIR""aclocal"
 echo "*** Recreating aclocal.m4"
 echo "$ACLOCAL"
-if $ACLOCAL --version | egrep "1\.6" >/dev/null 2>&1; then \
-        echo "aclocal 1.6" ;
-	$ACLOCAL;\
-elif $ACLOCAL --version | egrep "1\.5" >/dev/null 2>&1; then \
-        echo "aclocal 1.5" ;\
-	echo "You must have aclocal 1.6 to use aclocal with this \
-source";\
-elif $ACLOCAL --version | egrep "1\.4" >/dev/null 2>&1; then \
-        echo "aclocal 1.4" ;\
-	echo "You must have aclocal 1.6 to use aclocal with this \
-source";\
-else \
-        echo "aclocal unknown" ;\
-	echo "You must have aclocal 1.6 to use aclocal with this \
-source";\
-fi
+	$ACLOCAL;
+
 echo "*** Recreating configure"
 AUTOCONF="$AUTODIR""autoconf"
 AUTOHEAD="$AUTODIR""autoheader"
-if $AUTOCONF --version | egrep "2\.53" >/dev/null 2>&1; then \
-        echo "autoconf 2.53" ;
-	if egrep "^AM_CONFIG_HEADER" configure.ac >/dev/null 2>&1; then \
-        	echo "*** Creating config.h template" ;\
-	        $AUTOHEAD ;\
-	fi
-	$AUTOCONF;\
-elif $AUTOCONF --version | egrep "2\.52" >/dev/null 2>&1; then \
-        echo "autoconf 2.52" ;
-	if egrep "^AM_CONFIG_HEADER" configure.ac >/dev/null 2>&1; then \
-        	echo "*** Creating config.h template" ;\
-	        $AUTOHEAD ;\
-	fi
-	$AUTOCONF;\
-elif $AUTOCONF --version | egrep "2\.13$" >/dev/null 2>&1; then \
-        echo "autoconf 2.13" ;\
-	echo "You must have autoconf 2.52+ to use autoconf with this \
-source";\
-else \
-        echo "autoconf unknown" ;\
-	echo "You must have autoconf 2.52+ to use autoconf with this \
-source";\
-fi
+	$AUTOHEAD ;
+	$AUTOCONF;
+	
 echo "*** Recreating the Makefile.in files"
 AUTOMAKE="$AUTODIR""automake"
-if $AUTOMAKE --version | egrep "1\.6" >/dev/null 2>&1; then \
-        echo "automake 1.6" ;
-	$AUTOMAKE;\
-elif $AUTOMAKE --version | egrep "1\.5" >/dev/null 2>&1; then \
-        echo "automake 1.5" ;\
-	echo "You must have automake 1.6 to use automake with this \
-source";\
-elif $AUTOMAKE --version | egrep "1\.4" >/dev/null 2>&1; then \
-        echo "automake 1.4" ;\
-	echo "You must have automake 1.6 to use automake with this \
-source";\
-else \
-        echo "automake unknown" ;\
-	echo "You must have automake 1.6 to use automake with this \
-source";\
-fi
+	$AUTOMAKE --foreign;
