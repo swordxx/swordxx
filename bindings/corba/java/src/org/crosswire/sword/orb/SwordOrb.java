8f11c46d7b90c355c3b0c1bd748dae1693c0d990
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2003-05-04 08:40:13 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@1167 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- bindings/corba/java/src/org/crosswire/sword/orb/SwordOrb.java
+++ bindings/corba/java/src/org/crosswire/sword/orb/SwordOrb.java
@@ -4,19 +4,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.StringWriter;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionBindingEvent;
 
-class SwordOrb extends Object {
+class SwordOrb extends Object implements HttpSessionBindingListener {
 	static org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(new String[]{}, null);
-	static SwordOrb runningOrb = null;
 	String ior = null;
 
-	private static SWMgr attach() {
+	private SWMgr attach() {
 		SWMgr retVal = null;
 		try {
-			if (runningOrb != null) {
-				org.omg.CORBA.Object obj = orb.string_to_object(runningOrb.ior);
-				retVal = SWMgrHelper.narrow(obj);
-			}
+			org.omg.CORBA.Object obj = orb.string_to_object(ior);
+			retVal = SWMgrHelper.narrow(obj);
 		}
 		catch(org.omg.CORBA.SystemException e) {
 			e.printStackTrace();
@@ -24,16 +24,28 @@ class SwordOrb extends Object {
 		return retVal;
 	}
 
-	private SwordOrb(String ior) {
-		this.ior = ior;
+	public SwordOrb() {
 	}
 
+
+//	this doesn't seem to work.  Never seems to get called for me
 	protected void finalize() throws Throwable {
 System.out.println("finalizing");
-		try { if (runningOrb != null) getSWMgrInstance().terminate(); } catch (Exception e) {}	// we know this doesn't return property cuz we killed the orb! :)
+		try {
+			getSWMgrInstance().terminate();
+		}
+		catch (Exception e) {}	// we know this doesn't return property cuz we killed the orb! :)
 	}
 
-	private static void startOrb() {
+	public void valueBound(HttpSessionBindingEvent httpSessionBindingEvent) {}
+	public void valueUnbound(HttpSessionBindingEvent httpSessionBindingEvent) {
+		try {
+			getSWMgrInstance().terminate();
+		}
+		catch (Exception e) {}	// we know this doesn't return property cuz we killed the orb! :)
+	}
+
+	private void startOrb() {
 		try {
 			java.lang.Process p = Runtime.getRuntime().exec("./swordorbserver");
 
@@ -44,12 +56,12 @@ System.out.println("finalizing");
 			String line;
 			line = input.readLine();
 //		retVal = p.waitFor();
-			runningOrb = new SwordOrb(line);
+			ior = line;
 		}
 		catch (Exception e) {e.printStackTrace();}
 	}
 
-	public static SWMgr getSWMgrInstance() {
+	public SWMgr getSWMgrInstance() {
 		SWMgr retVal = null;
 		try {
 			retVal = attach();
@@ -64,17 +76,27 @@ System.out.println("finalizing");
 		return retVal;
 	}
 
+
+	public static SWMgr getSWMgrInstance(HttpSession session) {
+		SwordOrb orb = (SwordOrb)session.getAttribute("SwordOrb");
+		if (orb == null) {
+			orb = new SwordOrb();
+			session.setAttribute("SwordOrb", orb);
+		}
+		return orb.getSWMgrInstance();
+	}
+
+
 	public static void main(String args[]) {
-		SWMgr mgr = SwordOrb.getSWMgrInstance();
+		SWMgr mgr = new SwordOrb().getSWMgrInstance();
 
-			System.out.println("PrefixPath: " + mgr.getPrefixPath());
-			System.out.println("ConfigPath: " + mgr.getConfigPath());
-			ModInfo[] modInfoList = mgr.getModInfoList();
-			System.out.println("sequence size: " + modInfoList.length);
-			for (int i = 0; i < modInfoList.length; i++) {
-				System.out.println(modInfoList[i].name + ": " + modInfoList[i].type + ": " + modInfoList[i].lang);
-			}
-
+		System.out.println("PrefixPath: " + mgr.getPrefixPath());
+		System.out.println("ConfigPath: " + mgr.getConfigPath());
+		ModInfo[] modInfoList = mgr.getModInfoList();
+		System.out.println("sequence size: " + modInfoList.length);
+		for (int i = 0; i < modInfoList.length; i++) {
+			System.out.println(modInfoList[i].name + ": " + modInfoList[i].type + ": " + modInfoList[i].lang);
+		}
 	}
 
 }
