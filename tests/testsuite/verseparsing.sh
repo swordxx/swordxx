aa6fa86c16b277207162dcd561508bce7e7ecc4e
joachim <joachim@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2003-06-14 11:18:05 +0000

    updates
    git-svn-id: https://crosswire.org/svn/sword/trunk@1210 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/testsuite/verseparsing.sh
+++ tests/testsuite/verseparsing.sh
@@ -33,3 +33,10 @@
 ../parsekey "I.jn 2:3"
 ../parsekey "I. jn 2:3"
 
+### German test keys
+../parsekey "1. Johannes 2:3" "de"
+
+### Range parsing tests
+../parsekey "1. Johannes 1:1-3:10" "de"
+../parsekey "1. Joh 1:1-3:10" "de"
+../parsekey "1Jn 1:1-3:10"
