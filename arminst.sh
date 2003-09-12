70311388b7846cea989af12c86d9b44f7b39ac5e
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2003-09-12 23:10:56 +0000

    *** empty log message ***
    git-svn-id: https://crosswire.org/svn/sword/trunk@1386 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- arminst.sh
+++ arminst.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+OPTIONS="--host=arm-linux --prefix=/usr"
+OPTIONS="--disable-shared $OPTIONS"
+OPTIONS="--without-conf $OPTIONS"
+OPTIONS="--sysconfdir=/etc $OPTIONS"
+#OPTIONS="--with-vcl $OPTIONS"
+#OPTIONS="--enable-debug $OPTIONS"
+#OPTIONS="--enable-profile $OPTIONS"
+OPTIONS="--without-lucene $OPTIONS"
+OPTIONS="--without-curl $OPTIONS"
+
 export PATH=$PATH:/usr/local/arm/2.95.3/bin/
 #export CXXFLAGS="-I/usr/include/g++-3 -O3"
 export CXXFLAGS="-O3"
 export CFLAGS="-O3"
-./configure --host=arm-linux --prefix=/usr \
-	--disable-shared \
-	--without-conf --sysconfdir=/etc 
+
+CWD=`pwd`
+SRCBASE=`dirname $CWD`
+export QPEDIR=$SRCBASE/buildroot-oz/build/opie
+export QTDIR=$SRCBASE/buildroot-oz/build/qt-2.3.7
+export PATH=$QTDIR/bin:/usr/local/arm/bin:$PATH
+export TMAKEPATH=/opt/Qtopia/tmake/lib/qws/linux-ipaq-g++
+export CPPFLAGS="-I/usr/include/g++-3/ -I$SRCBASE/buildroot-oz/output/staging/include"
+export CFLAGS=$CPPFLAGS
+export CXXFLAGS=$CPPFLAGS
+export LDFLAGS=-L$SRCBASE/buildroot-oz/output/staging/target/lib
+
+CPPFLAGS="$CFLAGS $CPPFLAGS -DUSBINARY" ./configure $OPTIONS $*
+
 
 echo ""
 echo ""
