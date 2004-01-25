e877892a2427818704e0ae6e73dff1857bd7995b
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2004-01-25 01:03:35 +0000

    fixed utf8utf16 conversion a little (for greek and english, at least)
    git-svn-id: https://crosswire.org/svn/sword/trunk@1468 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/modules/filters/utf8utf16.cpp
+++ src/modules/filters/utf8utf16.cpp
@@ -14,67 +14,65 @@ SWORD_NAMESPACE_START
 UTF8UTF16::UTF8UTF16() {
 }
 
-
 char UTF8UTF16::processText(SWBuf &text, const SWKey *key, const SWModule *module) {
 	const unsigned char *from;
+	char digit[10];
+	unsigned long ch;
+        signed short utf16;
+	unsigned char from2[7];
 
-	int len;
-	unsigned long uchar, uchars[10];
-	unsigned char significantFirstBits, subsequent;
-	unsigned short schar;
-  
-	 if ((unsigned long)key < 2)	// hack, we're en(1)/de(0)ciphering
-		return -1;
-
-  
 	SWBuf orig = text;
+
 	from = (const unsigned char *)orig.c_str();
 
+	// -------------------------------
 	for (text = ""; *from; from++) {
-		uchar = 0;
+		ch = 0;
+                //case: ANSI
 		if ((*from & 128) != 128) {
-			//if (*from != ' ')
-			uchar = *from;
-		}
-		else if ((*from & 128) && ((*from & 64) != 64)) {
-			// error, do nothing
+			text.setSize(text.size()+2);
+			*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)*from;
 			continue;
 		}
-		else {
-			uchars[0] = *from;
-			uchars[0] <<= 1;
-			for (subsequent = 1; (uchars[0] & 128) && (subsequent < 10); subsequent++) {
-				uchars[0] <<= 1;
-				uchars[subsequent] = from[subsequent];
-				uchars[subsequent] &= 63;
-				uchar <<= 6;
-				uchar |= uchars[subsequent];
+                //case: Invalid UTF-8 (illegal continuing byte in initial position)
+		if ((*from & 128) && ((*from & 64) != 64)) {
+			continue;
+		}
+                //case: 2+ byte codepoint
+		from2[0] = *from;
+		from2[0] <<= 1;
+		int subsequent;
+		for (subsequent = 1; (from2[0] & 128) && (subsequent < 7); subsequent++) {
+			from2[0] <<= 1;
+			from2[subsequent] = from[subsequent];
+			from2[subsequent] &= 63;
+			ch <<= 6;
+			ch |= from2[subsequent];
+		}
+		subsequent--;
+		from2[0] <<= 1;
+		char significantFirstBits = 8 - (2+subsequent);
+		
+		ch |= (((short)from2[0]) << (((6*subsequent)+significantFirstBits)-8));
+		from += subsequent;
+			if (ch < 0x10000) {
+				text.setSize(text.size()+2);
+				*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)ch;
+			 }
+			else {
+				utf16 = (signed short)((ch - 0x10000) / 0x400 + 0xD800);
+				text.setSize(text.size()+2);
+				*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)utf16;
+				utf16 = (signed short)((ch - 0x10000) % 0x400 + 0xDC00);
+				text.setSize(text.size()+2);
+				*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)utf16;
 			}
-			subsequent--;
-			uchars[0] <<=1;
-			significantFirstBits = 8 - (2+subsequent);
-	 
-			uchar |= (((short)uchars[0]) << (((6*subsequent)+significantFirstBits)-8));
-			from += subsequent;
-		}
-
-		if (uchar < 0x1ffff) {
-			text.setSize(text.size()+2);
-			*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)uchar;
-		}
-		else {
-			uchar -= 0x10000;
-			schar = 0xD800 | (uchar & 0x03ff);
-			uchar >>= 10;
-			uchar |= 0xDC00;
-			text.setSize(text.size()+4);
-			*((unsigned short *)(text.getRawData()+(text.size()-4))) = (unsigned short)schar;
-			*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)uchar;
-		}
 	}
 	text.setSize(text.size()+2);
 	*((unsigned short *)(text.getRawData()+(text.size()-2))) = (unsigned short)0;
+	   
 	return 0;
+
 }
 
 SWORD_NAMESPACE_END
