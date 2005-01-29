c9453754b9b15d6ca9983a5efa88e5b5d4150348
jansorg <jansorg@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2005-01-29 21:45:41 +0000

    Added small testcase for UTF8 verse parsing
    git-svn-id: https://crosswire.org/svn/sword/trunk@1703 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ tests/testsuite/verseparsing-utf8.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This only works if --with-icu was passed to configure 
+# and if src/mgr/stringmgr.cpp has the ICUStringMgr instance enabled (commented out by default)
+
+### German test keys
+../parsekey "Matthäus 2:3-12" "de"
+../parsekey "Römer 2:13" "de"
+../parsekey "Matthäus 1:2-Röm 3:13" "de"
+../parsekey "1. Könige 2" "de"
+
+../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön" "de"
+../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön-2.Kön;I Kings-Matthäus" "de"
+../parsekey "Maleachi 1:1 - Matthäus 2:1" "de"
