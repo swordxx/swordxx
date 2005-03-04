77b5e9a517b2b5a7cad3953cd682c03ae08360f8
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2005-03-04 14:05:31 +0000

    Added a basic cipher test that kindof works
    git-svn-id: https://crosswire.org/svn/sword/trunk@1734 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ tests/ciphertest.cpp
@@ -0,0 +1,31 @@
+#include <cipherfil.h>
+#include <filemgr.h>
+#include <iostream>
+
+using namespace sword;
+
+int main(int argc, char **argv) {
+	
+	if (argc != 3) {
+		std::cerr << "usage: " << *argv << " <key> <0-encipher|1-decipher>\n";
+		return -1;
+	}
+
+	
+	int encipher = atoi(argv[2]);
+
+	SWFilter *filter = new CipherFilter(argv[1]);
+
+	SWBuf text;
+	char buf[4096];
+	std::cin >> buf;
+	text = buf;
+
+	filter->processText(text, (SWKey *)encipher);
+
+	std::cout << text;
+	
+
+	
+	return 0;
+}
