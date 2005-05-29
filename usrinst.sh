a3bedd946e6d668e09854e2dd676cea48826eb9f
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2005-05-29 04:46:43 +0000

    Packaged for 1.5.8
    git-svn-id: https://crosswire.org/svn/sword/trunk@1820 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- usrinst.sh
+++ usrinst.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 OPTIONS="--prefix=/usr $OPTIONS"
-OPTIONS="--disable-shared $OPTIONS"
 OPTIONS="--without-conf $OPTIONS"
 OPTIONS="--sysconfdir=/etc $OPTIONS"
-#OPTIONS="--with-icu $OPTIONS"
-#OPTIONS="--with-vcl $OPTIONS"
-OPTIONS="--enable-debug $OPTIONS"
-#OPTIONS="--enable-profile $OPTIONS"
 #OPTIONS="--with-lucene $OPTIONS"
+#OPTIONS="--with-icu $OPTIONS"
 #OPTIONS="--without-curl $OPTIONS"
 #OPTIONS="--enable-tests $OPTIONS"
-OPTIONS="--disable-utilities $OPTIONS"
+#OPTIONS="--enable-profile $OPTIONS"
+#OPTIONS="--enable-debug $OPTIONS"
+#OPTIONS="--disable-shared $OPTIONS"
+#OPTIONS="--disable-utilities $OPTIONS"
 
 
 CPPFLAGS="$CFLAGS $CPPFLAGS -DUSBINARY" ./configure $OPTIONS $*
