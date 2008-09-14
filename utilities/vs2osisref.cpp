49217510dd39fc0350d84bd99817a37fa6689364
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2008-09-14 02:02:33 +0000

    added locale support (to allow parsing of non-English verse refs)
    git-svn-id: https://crosswire.org/svn/sword/trunk@2199 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- utilities/vs2osisref.cpp
+++ utilities/vs2osisref.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <versekey.h>
+#include <localemgr.h>
 
 #ifndef NO_SWORD_NAMESPACE
 using namespace sword;
@@ -13,10 +14,17 @@ int main(int argc, char **argv)
 {
         if (argc < 2) {
         	cerr << argv[0] << " - parse verse reference to OSISRef markup\n";
-	        cerr << "usage: "<< argv[0] << " <verse_ref> [verse_context]\n";
+	        cerr << "usage: "<< argv[0] << " <verse ref> [verse context] [locale]\n";
         	cerr << "\n\n";
 	        exit(-1);
         }
+
+        char * locale;
+
+	if (argc > 3) {
+		LocaleMgr::getSystemLocaleMgr()->setDefaultLocaleName(argv[3]);
+	}
+
         VerseKey verseKey = (argc > 2) ? argv[2] : "Gen 1:1";
 
         std::cout << VerseKey::convertToOSIS(argv[1], &verseKey) << "\n";
