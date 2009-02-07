7ddd11711ea879ffdbcb7f398aac51cd3d0a3cdc
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2009-02-07 08:50:09 +0000

    license updates
    git-svn-id: https://crosswire.org/svn/sword/trunk@2239 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- bindings/corba/orbitcpp/testclient.cpp
+++ bindings/corba/orbitcpp/testclient.cpp
@@ -1,5 +1,23 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: f; c-basic-offset: 4 -*- */
 
+/*
+ * Copyright 2009 CrossWire Bible Society (http://www.crosswire.org)
+ *	CrossWire Bible Society
+ *	P. O. Box 2528
+ *	Tempe, AZ  85280-2528
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+
+
 #include "swordorb-cpp-stubs.h"
 #include "swordorb-cpp-common.h"
 #include <iostream>
