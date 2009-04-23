8ca5cde437bfe4727ac2b2fbc331db33788e262d
benpmorgan <benpmorgan@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2009-04-23 06:33:28 +0000

    Added Jonathan Marsden's abbreviations checking slightly changed
    git-svn-id: https://crosswire.org/svn/sword/trunk@2329 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/testsuite/verseparsing.sh
+++ tests/testsuite/verseparsing.sh
@@ -80,3 +80,24 @@
 ../parsekey "1Jn 1:1; 3:10"
 ../parsekey "1Jn 1:1 ;3:10"
 ../parsekey "1Jn 1:1 ; 3:10"
+
+en_abbrevs="
+Gen Genes Exod Ex Exo Lev Le Levi Num Nu Numb Deut Deu De Josh Jos Joshu
+Judg Jdg Judge Ruth Ru Rut 1Sam 1Sa 1Samu 2Sam 2Sa 2Samu 1Kgs 1Ki 1King
+2Kgs 2Ki 2King 1Chr 1Chron 1Ch 2Chr 2Chron 2Ch Ezra Ezr Neh Ne Nehe
+Esth Es Est Job  Ps Psa Psalm Prov Pr Pro Eccl Ec Ecc Song So Son
+Isa Is Isai Jer Jere Je Lam La Lament Ezek Ez Eze Dan Dan Da Hos Hos
+Hose Joel Joe Amos Am Amo Obad Ob Oba Jonah Jon Jona Mic Mi Mica Nah Na
+Nahu Hab Haba Ha Zeph Zep Hag Hagg Zech Ze Zecha Mal Mala
+Matt Mt Mat Mark Mar Ma Luke Lk Luk John Joh Jn Acts Ac A Rom Ro Roma
+1Cor 1Co 1Cori 2Cor 2Co 2Cori Gal Ga Gala Eph Ep Ephes Ph Phi Phil Phili Philip
+Col Co Colo 1Thess 1Th 1Thes 2Thess 2Th 2Thes 1Tim 1Ti 1Timo
+2Tim 2Ti 2Timo Titus Tit Titu Phlm Phile Heb Hebrews Hebr
+Jas James Jam 1Pet 1Pe 1Pete 2Pet 2Pe 2Pete 1John 1Jn 1J
+2John 2Jn 2J 3John 3Jn 3J Jude Jud Ju Rev Re Reve
+"
+for i in $en_abbrevs
+do
+  ../parsekey $i
+done
+
