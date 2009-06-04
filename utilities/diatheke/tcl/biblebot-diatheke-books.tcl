f0cc354ae6666d40af2d6b960275b104e8f23224
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2009-06-04 07:09:33 +0000

    added/cleaned up license text
    git-svn-id: https://crosswire.org/svn/sword/trunk@2429 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- utilities/diatheke/tcl/biblebot-diatheke-books.tcl
+++ utilities/diatheke/tcl/biblebot-diatheke-books.tcl
@@ -1,3 +1,19 @@
+# Diatheke/Tcl 5.0 by Chris Little <chrislit@crosswire.org>
+
+# Copyright 1999-2009 CrossWire Bible Society (http://www.crosswire.org)
+# 	CrossWire Bible Society
+# 	P. O. Box 2528
+# 	Tempe, AZ  85280-2528
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 bind pub - !KJV setver_KJV
 
 proc setver_KJV {nick uhost hand channel arg} {
--- utilities/diatheke/tcl/biblebot-diatheke.tcl
+++ utilities/diatheke/tcl/biblebot-diatheke.tcl
@@ -1,21 +1,19 @@
 # Diatheke/Tcl 5.0 by Chris Little <chrislit@crosswire.org>
 # Based on code schema of <cking@acy.digex.net>
 
-# *
-# * Copyright 1999-2009 CrossWire Bible Society (http://www.crosswire.org)
-# *	CrossWire Bible Society
-# *	P. O. Box 2528
-# *	Tempe, AZ  85280-2528
-# *
-# * This program is free software; you can redistribute it and/or modify it
-# * under the terms of the GNU General Public License as published by the
-# * Free Software Foundation version 2.
-# *
-# * This program is distributed in the hope that it will be useful, but
-# * WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# * General Public License for more details.
-# *
+# Copyright 1999-2009 CrossWire Bible Society (http://www.crosswire.org)
+# 	CrossWire Bible Society
+# 	P. O. Box 2528
+# 	Tempe, AZ  85280-2528
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU General Public License for more details.
 
 set diaver 5.0
 
