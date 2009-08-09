274400baa33fb904a005eb282c15aa0862a73e20
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2009-08-09 22:14:21 +0000

    added new test for assessment of our extended character set searching
    git-svn-id: https://crosswire.org/svn/sword/trunk@2438 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ tests/striptest.cpp
@@ -0,0 +1,32 @@
+#include <swmgr.h>
+#include <swmodule.h>
+#include <iostream>
+
+using namespace sword;
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv) {
+
+	SWBuf modName = (argc > 1) ? argv[2] : "WLC";
+	SWBuf keyText = (argc > 2) ? argv[3] : "Gen.1.9";
+	SWBuf searchText = (argc > 3) ? argv[4] : "מתחת";
+	SWMgr library;
+	SWModule *book = library.getModule(modName);
+	StringList filters = library.getGlobalOptions();
+	for (StringList::iterator it = filters.begin(); it != filters.end(); ++it) {
+	    // blindly turn off all filters.  Some filters don't support "Off", but that's ok, we should just silently fail on those.
+	    library.setGlobalOption(*it, "Off");
+	}
+	book->setKey(keyText.c_str());
+	SWBuf entryStripped = book->StripText();
+	cout << "Module: " << book->Description() << "\t Key: " << book->getKeyText() << "\n";
+	cout << "RawEntry:\n" << book->getRawEntry() << "\n";
+	cout << "StripText:\n" << entryStripped << "\n";
+	cout << "Search Target: " << searchText << "\n";
+	cout << "Search Target StripText: " << book->StripText(searchText) << "\n";
+	cout << "Found: " << ((strstr(entryStripped.c_str(), book->StripText(searchText))) ? "true":"false") << endl; 
+
+	return 0;
+}
