cb99ca88a756019036a1fceb704a63394b17afbd
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2010-04-20 21:23:01 +0000

    updated swaptest to use sword swap macros
    git-svn-id: https://crosswire.org/svn/sword/trunk@2515 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/swaptest.cpp
+++ tests/swaptest.cpp
@@ -19,8 +19,8 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("0x%.4x 0x%.4x\n", 255, __swap16(255));
-	printf("0x%.8x 0x%.8x\n", 255, __swap32(255));
+	printf("0x%.4x 0x%.4x\n", 255, swordtoarch16(255));
+	printf("0x%.8x 0x%.8x\n", 255, swordtoarch32(255));
 //	printf("0x%.16x 0x%.16llx\n", 255, __swap64(255));
 	return 0;
 }
