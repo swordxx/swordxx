5a3a650b6f0e7f2f313f379d75acd6c7cd1a9a75
chrislit <chrislit@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2010-10-02 21:08:35 +0000

    removed redundant data
    git-svn-id: https://crosswire.org/svn/sword/trunk@2557 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- include/canon_german.h
+++ include/canon_german.h
@@ -72,37 +72,6 @@ struct sbook otbooks_german[] = {
   {"", "", "", 0}
 };
 
-struct sbook ntbooks_german[] = {
-  {"Matthew", "Matt", "Matt", 28},
-  {"Mark", "Mark", "Mark", 16},
-  {"Luke", "Luke", "Luke", 24},
-  {"John", "John", "John", 21},
-  {"Acts", "Acts", "Acts", 28},
-  {"Romans", "Rom", "Rom", 16},
-  {"I Corinthians", "1Cor", "1Cor", 16},
-  {"II Corinthians", "2Cor", "2Cor", 13},
-  {"Galatians", "Gal", "Gal", 6},
-  {"Ephesians", "Eph", "Eph", 6},
-  {"Philippians", "Phil", "Phil", 4},
-  {"Colossians", "Col", "Col", 4},
-  {"I Thessalonians", "1Thess", "1Thess", 5},
-  {"II Thessalonians", "2Thess", "2Thess", 3},
-  {"I Timothy", "1Tim", "1Tim", 6},
-  {"II Timothy", "2Tim", "2Tim", 4},
-  {"Titus", "Titus", "Titus", 3},
-  {"Philemon", "Phlm", "Phlm", 1},
-  {"Hebrews", "Heb", "Heb", 13},
-  {"James", "Jas", "Jas", 5},
-  {"I Peter", "1Pet", "1Pet", 5},
-  {"II Peter", "2Pet", "2Pet", 3},
-  {"I John", "1John", "1John", 5},
-  {"II John", "2John", "2John", 1},
-  {"III John", "3John", "3John", 1},
-  {"Jude", "Jude", "Jude", 1},
-  {"Revelation of John", "Rev", "Rev", 22},
-  {"", "", "", 0}
-};
-
 /******************************************************************************
  *	Maximum verses per chapter
  */
