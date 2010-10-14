b555d3d00642a86e3c566613784a97fefc523c2d
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2010-10-14 15:38:46 +0000

    Updated tests to all pass again
    git-svn-id: https://crosswire.org/svn/sword/trunk@2570 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/testsuite/verseparsing-utf8.sh
+++ tests/testsuite/verseparsing-utf8.sh
@@ -21,11 +21,11 @@
 #
 
 ### German test keys
-../parsekey "Matthäus 2:3-12" "de"
-../parsekey "Römer 2:13" "de"
-../parsekey "Matthäus 1:2-Röm 3:13" "de"
-../parsekey "1. Könige 2" "de"
+../parsekey "Matthäus 2:3-12" "de" KJV ge 1
+../parsekey "Römer 2:13" "de" KJV ge 1
+../parsekey "Matthäus 1:2-Röm 3:13" "de" KJV ge 1
+../parsekey "1. Könige 2" "de" KJV ge 1
 
-../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön" "de"
-../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön-2.Kön;I Kings-Matthäus" "de"
-../parsekey "Maleachi 1:1 - Matthäus 2:1" "de"
+../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön" "de" KJV ge 1
+../parsekey "1. Könige - 2. Könige; Markus 1:1; Matthäus 2:1; Matthäus; 1.Kön-2.Kön;I Kings-Matthäus" "de" KJV ge 1
+../parsekey "Maleachi 1:1 - Matthäus 2:1" "de" KJV ge 1
--- tests/testsuite/verseparsing.sh
+++ tests/testsuite/verseparsing.sh
@@ -18,80 +18,80 @@
 # General Public License for more details.
 #
 
-../parsekey "1jn.2.3"
-../parsekey "1 jn.2.3"
-../parsekey "Ijn.2.3"
-../parsekey "I jn.2.3"
-../parsekey "1jn 2.3"
-../parsekey "1 jn 2.3"
-../parsekey "Ijn 2.3"
-../parsekey "I jn 2.3"
-../parsekey "1jn.2:3"
-../parsekey "1 jn.2:3"
-../parsekey "Ijn.2:3"
-../parsekey "I jn.2:3"
-../parsekey "1jn 2:3"
-../parsekey "1 jn 2:3"
-../parsekey "Ijn 2:3"
-../parsekey "I jn 2:3"
+../parsekey "1jn.2.3" en KJV ge 1
+../parsekey "1 jn.2.3" en KJV ge 1
+../parsekey "Ijn.2.3" en KJV ge 1
+../parsekey "I jn.2.3" en KJV ge 1
+../parsekey "1jn 2.3" en KJV ge 1
+../parsekey "1 jn 2.3" en KJV ge 1
+../parsekey "Ijn 2.3" en KJV ge 1
+../parsekey "I jn 2.3" en KJV ge 1
+../parsekey "1jn.2:3" en KJV ge 1
+../parsekey "1 jn.2:3" en KJV ge 1
+../parsekey "Ijn.2:3" en KJV ge 1
+../parsekey "I jn.2:3" en KJV ge 1
+../parsekey "1jn 2:3" en KJV ge 1
+../parsekey "1 jn 2:3" en KJV ge 1
+../parsekey "Ijn 2:3" en KJV ge 1
+../parsekey "I jn 2:3" en KJV ge 1
 
-../parsekey "1.jn.2.3"
-../parsekey "1. jn.2.3"
-../parsekey "I.jn.2.3"
-../parsekey "I. jn.2.3"
-../parsekey "1.jn 2.3"
-../parsekey "1. jn 2.3"
-../parsekey "I.jn 2.3"
-../parsekey "I. jn 2.3"
-../parsekey "1.jn.2:3"
-../parsekey "1. jn.2:3"
-../parsekey "I.jn.2:3"
-../parsekey "I. jn.2:3"
-../parsekey "1.jn 2:3"
-../parsekey "1. jn 2:3"
-../parsekey "I.jn 2:3"
-../parsekey "I. jn 2:3"
+../parsekey "1.jn.2.3" en KJV ge 1
+../parsekey "1. jn.2.3" en KJV ge 1
+../parsekey "I.jn.2.3" en KJV ge 1
+../parsekey "I. jn.2.3" en KJV ge 1
+../parsekey "1.jn 2.3" en KJV ge 1
+../parsekey "1. jn 2.3" en KJV ge 1
+../parsekey "I.jn 2.3" en KJV ge 1
+../parsekey "I. jn 2.3" en KJV ge 1
+../parsekey "1.jn.2:3" en KJV ge 1
+../parsekey "1. jn.2:3" en KJV ge 1
+../parsekey "I.jn.2:3" en KJV ge 1
+../parsekey "I. jn.2:3" en KJV ge 1
+../parsekey "1.jn 2:3" en KJV ge 1
+../parsekey "1. jn 2:3" en KJV ge 1
+../parsekey "I.jn 2:3" en KJV ge 1
+../parsekey "I. jn 2:3" en KJV ge 1
 
 ### German test keys
-../parsekey "1. Johannes 2:3" "de"
+../parsekey "1. Johannes 2:3" "de" KJV ge 1
 
 ### Range parsing tests
-../parsekey "1. Johannes 1:1-3:10" "de"
-../parsekey "1. Joh 1:1-3:10" "de"
-../parsekey "1Jn 1:1-3:10"
-../parsekey "1. Johannes 1:1 - 3:10" "de"
-../parsekey "1. Joh 1:1 - 3:10" "de"
-../parsekey "1Jn 1:1 - 3:10"
-../parsekey "1. Johannes 1:1 -3:10" "de"
-../parsekey "1. Joh 1:1 -3:10" "de"
-../parsekey "1Jn 1:1 -3:10"
-../parsekey "1. Johannes 1:1- 3:10" "de"
-../parsekey "1. Joh 1:1- 3:10" "de"
-../parsekey "1Jn 1:1- 3:10"
+../parsekey "1. Johannes 1:1-3:10" "de" KJV ge 1
+../parsekey "1. Joh 1:1-3:10" "de" KJV ge 1
+../parsekey "1Jn 1:1-3:10" en KJV ge 1
+../parsekey "1. Johannes 1:1 - 3:10" "de" KJV ge 1
+../parsekey "1. Joh 1:1 - 3:10" "de" KJV ge 1
+../parsekey "1Jn 1:1 - 3:10" en KJV ge 1
+../parsekey "1. Johannes 1:1 -3:10" "de" KJV ge 1
+../parsekey "1. Joh 1:1 -3:10" "de" KJV ge 1
+../parsekey "1Jn 1:1 -3:10" en KJV ge 1
+../parsekey "1. Johannes 1:1- 3:10" "de" KJV ge 1
+../parsekey "1. Joh 1:1- 3:10" "de" KJV ge 1
+../parsekey "1Jn 1:1- 3:10" en KJV ge 1
 
 ### List parsing tests
-../parsekey "1Jn 1:1 3:10"
-../parsekey "1Jn 1:1  3:10"
-../parsekey "1Jn 1:1,3:10"
-../parsekey "1Jn 1:1, 3:10"
-../parsekey "1Jn 1:1 ,3:10"
-../parsekey "1Jn 1:1 , 3:10"
-../parsekey "1Jn 1:1;3:10"
-../parsekey "1Jn 1:1; 3:10"
-../parsekey "1Jn 1:1 ;3:10"
-../parsekey "1Jn 1:1 ; 3:10"
-../parsekey "1Jn 1:1a ; 3:10b"
+../parsekey "1Jn 1:1 3:10" en KJV ge 1
+../parsekey "1Jn 1:1  3:10" en KJV ge 1
+../parsekey "1Jn 1:1,3:10" en KJV ge 1
+../parsekey "1Jn 1:1, 3:10" en KJV ge 1
+../parsekey "1Jn 1:1 ,3:10" en KJV ge 1
+../parsekey "1Jn 1:1 , 3:10" en KJV ge 1
+../parsekey "1Jn 1:1;3:10" en KJV ge 1
+../parsekey "1Jn 1:1; 3:10" en KJV ge 1
+../parsekey "1Jn 1:1 ;3:10" en KJV ge 1
+../parsekey "1Jn 1:1 ; 3:10" en KJV ge 1
+../parsekey "1Jn 1:1a ; 3:10b" en KJV ge 1
 
 ### These two are not quite right yet
-../parsekey "1Jn 1:1-2a; 3:10b-11"
-../parsekey "1Jn 1:1c;3:10d-12e,13"
+../parsekey "1Jn 1:1-2a; 3:10b-11" en KJV ge 1
+../parsekey "1Jn 1:1c;3:10d-12e,13" en KJV ge 1
 
-../parsekey "1Jn1.1f; 3:10ff"
-../parsekey "1Jn1.1ff;3:10f"
-../parsekey "1Jn1.1,8ff; 3:10-12,14f,17ff"
+../parsekey "1Jn1.1f; 3:10ff" en KJV ge 1
+../parsekey "1Jn1.1ff;3:10f" en KJV ge 1
+../parsekey "1Jn1.1,8ff; 3:10-12,14f,17ff" en KJV ge 1
 
 ### false negative
-../parsekey "Song of 3:5"
+../parsekey "Song of 3:5" en KJV ge 1
 
 en_abbrevs="
 Gen Genes Exod Ex Exo Lev Le Levi Num Nu Numb Deut Deu De Josh Jos Joshu
@@ -110,6 +110,6 @@ Jas James Jam 1Pet 1Pe 1Pete 2Pet 2Pe 2Pete 1John 1Jn 1J
 "
 for i in $en_abbrevs
 do
-  ../parsekey $i
+  ../parsekey $i en KJV ge 1
 done
 
