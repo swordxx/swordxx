5c1b08509c13f9c5f1572a3c3f81310a57596ff0
greg.hellings <greg.hellings@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2010-12-07 14:33:19 +0000

    Merging a pair of sample CMake invocation scripts.
    git-svn-id: https://crosswire.org/svn/sword/trunk@2593 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ cmake/build-debug.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# A sample script showing how to build most of the options available to this system.
+# Invoke it from the top directory of SWORD by calling $ cmake/build-debug.sh
+#
+
+mkdir -p build
+cd build
+# Configure with Python and Perl bindings, examples, tests and a debug build into
+# a shared library
+cmake -DSWORD_BINDINGS="Python Perl" \
+	-DSWORD_BUILD_EXAMPLES="Yes" \
+	-DSWORD_BUILD_TESTS="Yes" \
+	-DLIBSWORD_LIBRARY_TYPE="Shared" \
+	-DCMAKE_BUILD_TYPE="Debug" ..
+make -j5
+cd bindings/swig/perl
+make
+cd ../python
+python setup.py build
+
+echo "Now the library has been built, along with the Perl and Python bindings. \
+	If you would like to install, you may now do so by changing into the build \
+	directory and typing \"sudo make install\".  Python bindings may be installed \
+	by then changing into tye build/bindings/swig/python diretory and typing \"sudo python \
+	setup.py install\" and Perl bindings may be installed by changing into the \
+	build/bindings/swig/perl directory and issuing the command \"sudo make install\"."
