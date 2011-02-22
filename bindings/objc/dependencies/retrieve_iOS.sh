9afd001d1fa2ea53d881335dd59ad0775915f3ff
mdbergmann <mdbergmann@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2011-02-22 17:30:58 +0000

    added retrieve script which downloads dependencies for iOS project.
    git-svn-id: https://crosswire.org/svn/sword/trunk@2603 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ bindings/objc/dependencies/retrieve_iOS.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# this script downloads dependencies for iOS project
+
+curl -o libcurl_build.tar.gz http://www.crosswire.org/~mbergmann/libcurl_build.tar.gz
+tar -xzf libcurl_build.tar.gz
