a083a719b0f7a8da5349a6f1586f904857e3daba
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2011-09-29 17:29:26 +0000

    Added sane parameter parsing to imp2ld
    git-svn-id: https://crosswire.org/svn/sword/trunk@2657 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- utilities/imp2ld.cpp
+++ utilities/imp2ld.cpp
@@ -27,6 +27,7 @@
 #include <rawld4.h>
 #include <zld.h>
 #include <zipcomprs.h>
+#include <lzsscomprs.h>
 #include <stdio.h>
 
 using std::string;
@@ -35,78 +36,138 @@ using std::string;
 using namespace sword;
 #endif
 
+void usage(const char *progName, const char *error = 0) {
+	if (error) fprintf(stderr, "\n%s: %s\n", progName, error);
+	fprintf(stderr, "\n=== imp2ld (Revision $Rev: 2234 $) SWORD lexicon importer.\n");
+	fprintf(stderr, "\nusage: %s <imp_file> [options]\n", progName);
+	fprintf(stderr, "  -a\t\t\t augment module if exists (default is to create new)\n");
+	fprintf(stderr, "  -z\t\t\t use ZIP compression (default no compression)\n");
+	fprintf(stderr, "  -Z\t\t\t use LZSS compression (default no compression)\n");
+	fprintf(stderr, "  -o <output_path>\t where to write data files.\n");
+	fprintf(stderr, "  -4\t\t\t use 4 byte size entries (default is 2).\n");
+	fprintf(stderr, "  -b <entry_count>\t\t compression block size (default 30 entries)\n");
+	fprintf(stderr, "  -s\t\t\t case sensitive keys (default is not case sensitive)\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "'imp' format is a simple standard for importing data into SWORD modules.\n"
+		"Required is a plain text file containing $$$key lines followed by content.\n\n"
+		"$$$Abraham\n"
+		"Abraham was the father of Isaac...\n"
+		"He was called by God to leave his country and journey to the land of Canaan...\n"
+		"$$$Isaac\n"
+		"Isaac was the son of Abraham and Sarah...\n\n");
+	exit(-1);
+}
 
 int main(int argc, char **argv) {
 
-	const char * helptext ="imp2ld 1.0 Lexicon/Dictionary/Daily Devotional/Glossary module creation tool for the SWORD Project\n  usage:\n   %s <filename> [modname] [ 4 (default) | 2 | z - module driver] [entries per compression block] [true - caseSensitiveKeys]\n";
-
+	std::vector<string> linkbuffer;
 	signed long i = 0;
 	string keybuffer;
 	string entbuffer;
 	string linebuffer;
-	string modname;
 	char links = 0;
+	string modname;
+	SWBuf outPath          = "";
+	bool append            = false;
 	long blockCount = 30;
 	bool caseSensitive = false;
-	std::vector<string> linkbuffer;
+	SWCompress *compressor = 0;
+	SWBuf compType         = "";
+	bool fourByteSize      = false;
 
-	if (argc > 2) {
-		modname = argv[2];
+	if (argc < 2) usage(*argv);
+
+	const char *progName   = argv[0];
+	const char *inFileName = argv[1];
+
+	for (int i = 2; i < argc; i++) {
+		if (!strcmp(argv[i], "-a")) {
+			append = true;
+		}
+		else if (!strcmp(argv[i], "-z")) {
+			if (compType.size()) usage(*argv, "Cannot specify both -z and -Z");
+			if (fourByteSize) usage(*argv, "Cannot specify both -z and -4");
+			compType = "ZIP";
+		}
+		else if (!strcmp(argv[i], "-Z")) {
+			if (compType.size()) usage(*argv, "Cannot specify both -z and -Z");
+			if (fourByteSize) usage(*argv, "Cannot specify both -Z and -4");
+			compType = "LZSS";
+		}
+		else if (!strcmp(argv[i], "-4")) {
+			fourByteSize = true;
+		}
+		else if (!strcmp(argv[i], "-b")) {
+			if (i+1 < argc) {
+				blockCount = atoi(argv[++i]);
+				if (blockCount > 0) continue;
+			}
+			usage(*argv, "-b requires in entry count integer > 0");
+		}
+		else if (!strcmp(argv[i], "-o")) {
+			if (i+1 < argc) outPath = argv[++i];
+			else usage(progName, "-o requires <output_path>");
+		}
+		else if (!strcmp(argv[i], "-s")) {
+			caseSensitive = true;
+		}
+		else usage(progName, (((SWBuf)"Unknown argument: ")+ argv[i]).c_str());
 	}
-	else if (argc > 1) {
-		for (i = 0; (i < 16) && (argv[1][i]) && (argv[1][i] != '.'); i++) {
-			modname += argv[1][i];
+
+
+
+	if (outPath.size() < 1) {
+		for (i = 0; (i < 16) && (inFileName[i]) && (inFileName[i] != '.'); i++) {
+			outPath += inFileName[i];
 		}
 	}
-	else {
-		fprintf(stderr, helptext, argv[0]);
-		exit(-1);
-	}
 
-	std::ifstream infile(argv[1]);
+	std::ifstream infile(inFileName);
 
-	char mode = 1;
-	if (argc > 3) {
-		switch (*argv[3]) {
-			case 'z': mode = 3; break;
-			case '2': mode = 2; break;
-			default: mode = 1;
-		}
-	}
-
-	if (argc > 4) {
-		long bcTemp = atoi(argv[4]);
-		if (bcTemp > 0) {
-			blockCount = bcTemp;
-		}
-	}
-
-	if (argc > 5) {
-		caseSensitive = !sword::stricmp(argv[4], "true");
-	}
 
 	SWModule *mod = 0;
 	SWKey *key, *linkKey;
 
-	switch (mode) {
-	case 3:
+	if (compType == "ZIP") {
 #ifndef EXCLUDEZLIB
-		zLD::createModule(modname.c_str());
-		mod = new zLD(modname.c_str(), 0, 0, blockCount, new ZipCompress(), 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive);
+		compressor = new ZipCompress();
 #else
-		fprintf(stderr, "ERROR: %s: SWORD library not compiled with ZIP compression support.\n\tBe sure libzip is available when compiling SWORD library", *argv);
-		exit(-2);
+		usage(*argv, "ERROR: SWORD library not compiled with ZIP compression support.\n\tBe sure libzip is available when compiling SWORD library");
 #endif
-		break;
-	case 2:
-		RawLD::createModule(modname.c_str());
-		mod = new RawLD(modname.c_str(), 0, 0, 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive);
-		break;
-	case 1:
-		RawLD4::createModule(modname.c_str());
-		mod = new RawLD4(modname.c_str(), 0, 0, 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive);
-		break;
 	}
+	else if (compType == "LZSS") {
+		compressor = new LZSSCompress();
+	}
+
+	// setup module
+	if (!append) {
+		if (compressor) {
+			if (zLD::createModule(outPath)) {
+				fprintf(stderr, "ERROR: %s: couldn't create module at path: %s \n", *argv, outPath.c_str());
+				exit(-1);
+			}
+		}
+		else {
+			if (!fourByteSize)
+				RawLD::createModule(outPath);
+			else	RawLD4::createModule(outPath);
+		}
+	}
+
+	SWModule *module = 0;
+	if (compressor) {
+		// Create a compressed text module allowing very large entries
+		// Taking defaults except for first, fourth, fifth and last argument
+		mod = new zLD(outPath, 0, 0, blockCount, compressor, 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive);
+	}
+	else {
+		module = (!fourByteSize)
+			? (SWModule *)new RawLD (outPath, 0, 0, 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive)
+			: (SWModule *)new RawLD4(outPath, 0, 0, 0, ENC_UNKNOWN, DIRECTION_LTR, FMT_UNKNOWN, 0, caseSensitive);
+	}
+
+
+
 
 	key = mod->CreateKey();
 	linkKey = mod->CreateKey();
