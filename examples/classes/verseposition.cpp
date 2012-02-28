904456d2dcfb7629c9141c0ccc5b49fab340b79d
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2012-02-28 16:18:40 +0000

    added example to show positioning a verse
    git-svn-id: https://crosswire.org/svn/sword/trunk@2685 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ examples/classes/verseposition.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+#include <swmgr.h>
+#include <swmodule.h>
+#include <versekey.h>
+
+using namespace sword;
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	const char *modName = "HunKar";
+
+	SWMgr library;
+
+	SWModule *book = library.getModule("HunKar");
+
+	if (!book) {
+		cerr << "Can't find module: " << modName << endl;
+		return -1;
+	}
+
+	VerseKey *key = (VerseKey *)book->CreateKey();
+
+	key->setPosition(TOP);
+
+	cout << *key << endl;
+
+	key->Headings(true);
+
+	key->setPosition(TOP);
+
+	cout << *key << endl;
+
+	delete key;
+
+	return 0;
+}
