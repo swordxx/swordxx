07000238f7480f5ecdd81112ce9bee32a5f48d11
greg.hellings <greg.hellings@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2013-06-19 14:01:28 +0000

    Add greater flexibility on install and move more option handling into the options.cmake file for ease of access.
    git-svn-id: https://crosswire.org/svn/sword/trunk@2824 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,21 @@
 # A list of the options that the library supports
-OPTION(SWORD_PYTHON_INSTALL_DIR "Directory where the Python bindings will be installed. Defaults to default Python path."
-	"")
+SET(SWORD_PYTHON_INSTALL_DIR "" CACHE STRING "Directory where the Python bindings will be installed. Defaults to default Python path.")
+
+# Installation options
+IF(APPLE)
+	SET(SWORD_INSTALL_DIR "/opt/local")
+ELSEIF(MSVC)
+	SET(SWORD_INSTALL_DIR "C:\\Program Files (x86)\\libsword\\")
+ELSE(APPLE)
+	SET(SWORD_INSTALL_DIR "/usr/local")
+ENDIF(APPLE)
+
+IF(CMAKE_INSTALL_PREFIX)
+	SET(PREFIX "${CMAKE_INSTALL_PREFIX}" CACHE STRING "Directory into which to install architecture-dependent files. Defaults to ${SWORD_INSTALL_DIR}.")
+ELSE(CMAKE_INSTALL_PREFIX)
+	OPTION(PREFIX "${SWORD_INSTALL_DIR}" CACHE STRING "Directory into which to install architecture-dependent files. Defaults to ${SWORD_INSTALL_DIR}")
+ENDIF(CMAKE_INSTALL_PREFIX)
+
+SET(LIBDIR "${PREFIX}/lib" CACHE STRING "Object code library install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/lib")
+
+SET(INCLUDEDIR "${PREFIX}/include" CACHE STRING "C Header files install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/include.")
