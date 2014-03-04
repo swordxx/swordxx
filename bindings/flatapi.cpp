7636591adbe3e196d60e32d8350adc25561fdccb
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2014-03-04 01:38:30 +0000

    added headers back in and updated copyright date
    git-svn-id: https://crosswire.org/svn/sword/trunk@3061 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- bindings/flatapi.cpp
+++ bindings/flatapi.cpp
@@ -1,3 +1,26 @@
+/******************************************************************************
+ *
+ *  flatapi.cpp -	This file contains an api usable by non-C++
+ *			environments
+ *
+ * $Id$
+ *
+ * Copyright 2002-2014 CrossWire Bible Society (http://www.crosswire.org)
+ *	CrossWire Bible Society
+ *	P. O. Box 2528
+ *	Tempe, AZ  85280-2528
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+
 #include <iostream>
 #include <vector>
 #include <map>
--- include/flatapi.h
+++ include/flatapi.h
@@ -1,5 +1,27 @@
-#ifndef SWORDMINFLAT_H
-#define SWORDMINFLAT_H
+/******************************************************************************
+ *
+ *  flatapi.h -	This file contains an api usable by non-C++ environments
+ *
+ * $Id$
+ *
+ * Copyright 2002-2014 CrossWire Bible Society (http://www.crosswire.org)
+ *	CrossWire Bible Society
+ *	P. O. Box 2528
+ *	Tempe, AZ  85280-2528
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+
+#ifndef SWORDFLATAPI_H
+#define SWORDFLATAPI_H
 
 #include <inttypes.h>
 #include <defs.h>
