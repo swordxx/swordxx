9ed770076259a941015af757506ab4eaec9b1cb9
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2014-04-13 23:35:53 +0000

    added new test for bug MOD-61.  Make this test pass
    git-svn-id: https://crosswire.org/svn/sword/trunk@3152 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ tests/testsuite/ldr12n.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf ldr12n/
+mkdir -p ldr12n/mods.d
+mkdir -p ldr12n/modules
+
+cat > ldr12n/mods.d/ldr12n.conf <<!
+[ldr12n]
+DataPath=./modules/ldr12n
+ModDrv=RawLD
+Encoding=UTF-8
+SourceType=Plain
+Lang=en
+!
+
+../../utilities/imp2ld ldr12n.imp -o ldr12n/modules/ldr12n 2>&1 | grep -v \$Rev
+
+cd ldr12n && ../../ldtest ldr12n
