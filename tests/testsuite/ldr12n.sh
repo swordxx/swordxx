c37b3af0eecfb22f0d981b9d1496dafaa37ee05b
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2014-05-01 05:15:08 +0000

    updated test to check both padded and non-padded lexdict mods
    git-svn-id: https://crosswire.org/svn/sword/trunk@3221 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/testsuite/ldr12n.sh
+++ tests/testsuite/ldr12n.sh
@@ -14,6 +14,17 @@ Lang=en
 StrongsPadding=false
 !
 
-../../utilities/imp2ld ldr12n.imp -P -o ldr12n/modules/ldr12n 2>&1 | grep -v \$Rev
+cat > ldr12n/mods.d/ldr12np.conf <<!
+[ldr12np]
+DataPath=./modules/ldr12np
+ModDrv=RawLD
+Encoding=UTF-8
+SourceType=Plain
+Lang=en
+StrongsPadding=true
+!
 
-cd ldr12n && ../../ldtest ldr12n
+../../utilities/imp2ld ldr12n.imp -P -o ldr12n/modules/ldr12n 2>&1 | grep -v \$Rev
+../../utilities/imp2ld ldr12n.imp -o ldr12n/modules/ldr12np 2>&1 | grep -v \$Rev
+
+cd ldr12n && ../../ldtest ldr12n && ../../ldtest ldr12np
