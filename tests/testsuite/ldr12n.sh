9c6f236f9c9de17580fcd096326af7aba6cb89cc
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2014-05-01 05:14:35 +0000

    fix ld regularization test
    git-svn-id: https://crosswire.org/svn/sword/trunk@3219 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- tests/testsuite/ldr12n.sh
+++ tests/testsuite/ldr12n.sh
@@ -11,8 +11,9 @@ ModDrv=RawLD
 Encoding=UTF-8
 SourceType=Plain
 Lang=en
+StrongsPadding=false
 !
 
-../../utilities/imp2ld ldr12n.imp -o ldr12n/modules/ldr12n 2>&1 | grep -v \$Rev
+../../utilities/imp2ld ldr12n.imp -P -o ldr12n/modules/ldr12n 2>&1 | grep -v \$Rev
 
 cd ldr12n && ../../ldtest ldr12n
