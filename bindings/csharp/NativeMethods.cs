753044983786466b136f1b5be00967ba0182ebae
trampster <trampster@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2014-09-27 07:23:47 +0000

    Fix calling convention and removed .so from dllname so the bindings work on windows.
    git-svn-id: https://crosswire.org/svn/sword/trunk@3258 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- bindings/csharp/NativeMethods.cs
+++ bindings/csharp/NativeMethods.cs
@@ -73,53 +73,51 @@ namespace Sword
 	
 	public static class NativeMethods
 	{
+		public const string DLLNAME = "libsword";
 		
-		
-		public const string DLLNAME = "libsword.so";
-		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWMgr_new();
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWMgr_newWithPath(string path);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWMgr_getModuleByName(IntPtr hSWMgr, string moduleName);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void org_crosswire_sword_SWModule_setKeyText(IntPtr hSWModule, string key);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWModule_renderText(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWModule_getRawEntry(IntPtr hSWModule);
 
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWModule_getKeyText(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void org_crosswire_sword_SWMgr_delete(IntPtr hSWMgr);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWMgr_getModInfoList(IntPtr hSWMgr);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWModule_stripText(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void org_crosswire_sword_SWModule_setRawEntry(IntPtr hSWModule, string entryBuffer);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void org_crosswire_sword_SWModule_terminateSearch(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern char org_crosswire_sword_SWModule_popError(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern long org_crosswire_sword_SWModule_getEntrySize(IntPtr hSWModule);
 		
-		[DllImport(DLLNAME)]
+		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr org_crosswire_sword_SWModule_getEntryAttribute(IntPtr hSWModule, string level1, string level2, string level3, char filteredBool);
 		
 		[DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
