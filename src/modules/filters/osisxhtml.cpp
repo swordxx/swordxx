6376db27a9ec3a16cdb871c450e97a844c38bcc4
charcoal <charcoal@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2015-01-05 02:12:03 +0000

    fix type "sup" as type "super"
    git-svn-id: https://crosswire.org/svn/sword/trunk@3318 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/modules/filters/osisxhtml.cpp
+++ src/modules/filters/osisxhtml.cpp
@@ -617,7 +617,7 @@ bool OSISXHTML::handleToken(SWBuf &buf, const char *token, BasicFilterUserData *
 				else if (type == "ol") {
 					outText("</span>", buf, u);
 				}
-				else if (type == "sup") {
+				else if (type == "super") {
 					outText("</sup>", buf, u);
 				}
 				else if (type == "sub") {
