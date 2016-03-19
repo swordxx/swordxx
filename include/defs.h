426e202daec166fb6d2d61a9f92009c096798d53
Jaak Ristioja <jaak@ristioja.ee>
2016-03-19 11:34:04 +0200

    defs.h: Removed unused and empty SWDLLEXPORT_CTORFN macro.

--- include/defs.h
+++ include/defs.h
@@ -33,15 +33,12 @@ namespace swordxx {
 #  ifdef SWMAKINGDLL
 #    define SWDLLEXPORT _declspec( dllexport )
 #    define SWDLLEXPORT_DATA(type) _declspec( dllexport ) type
-#    define SWDLLEXPORT_CTORFN
 #  elif defined(SWUSINGDLL)
 #    define SWDLLEXPORT _declspec( dllimport )
 #    define SWDLLEXPORT_DATA(type) _declspec( dllimport ) type
-#    define SWDLLEXPORT_CTORFN
 #  else
 #    define SWDLLEXPORT
 #    define SWDLLEXPORT_DATA(type) type
-#    define SWDLLEXPORT_CTORFN
 #  endif
 
 #elif defined(__SWPM__)
@@ -49,15 +46,12 @@ namespace swordxx {
 #  ifdef SWMAKINGDLL
 #    define SWDLLEXPORT _Export
 #    define SWDLLEXPORT_DATA(type) _Export type
-#    define SWDLLEXPORT_CTORFN
 #  elif defined(SWUSINGDLL)
 #    define SWDLLEXPORT _Export
 #    define SWDLLEXPORT_DATA(type) _Export type
-#    define SWDLLEXPORT_CTORFN
 #  else
 #    define SWDLLEXPORT
 #    define SWDLLEXPORT_DATA(type) type
-#    define SWDLLEXPORT_CTORFN
 #  endif
 
 #elif defined(__GNUWIN32__)
@@ -65,27 +59,22 @@ namespace swordxx {
 #  ifdef SWMAKINGDLL
 #    define SWDLLEXPORT __declspec( dllexport )
 #    define SWDLLEXPORT_DATA(type) __declspec( dllexport ) type
-#    define SWDLLEXPORT_CTORFN
 #  elif defined(SWUSINGDLL)
 #    define SWDLLEXPORT __declspec( dllimport )
 #    define SWDLLEXPORT_DATA(type) __declspec( dllimport ) type
-#    define SWDLLEXPORT_CTORFN
 #  else
 #    define SWDLLEXPORT
 #    define SWDLLEXPORT_DATA(type) type
-#    define SWDLLEXPORT_CTORFN
 #  endif
 
 #elif defined(__GNUC__)
 
 #  define SWDLLEXPORT
 #  define SWDLLEXPORT_DATA(type) type
-#  define SWDLLEXPORT_CTORFN
 
 #else
 #  define SWDLLEXPORT
 #  define SWDLLEXPORT_DATA(type) type
-#  define SWDLLEXPORT_CTORFN
 #endif
 
 
