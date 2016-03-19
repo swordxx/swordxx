e895d9a75554b9c9cc65d6f4ed59ad6dfbd7258a
Jaak Ristioja <jaak@ristioja.ee>
2016-03-19 11:13:34 +0200

    RawStr: Fixed pointer checks.
    One of these was outright faulty.

--- src/modules/common/rawstr.cpp
+++ src/modules/common/rawstr.cpp
@@ -74,7 +74,7 @@ RawStr::RawStr(const char *ipath, int fileMode, bool caseSensitive) : caseSensit
 	buf.setFormatted("%s.dat", path);
 	datfd = FileMgr::getSystemFileMgr()->open(buf, fileMode, true);
 
-	if (datfd < 0) {
+    if (!datfd) {
 		SWLog::getSystemLog()->logError("%d", errno);
 	}
 
@@ -111,7 +111,7 @@ void RawStr::getIDXBufDat(long ioffset, char **buf) const
 {
 	int size;
 	char ch;
-	if (datfd > 0) {
+    if (datfd) {
 		datfd->seek(ioffset, SEEK_SET);
 		for (size = 0; datfd->read(&ch, 1) == 1; size++) {
 			if ((ch == '\\') || (ch == 10) || (ch == 13))
@@ -145,7 +145,7 @@ void RawStr::getIDXBuf(long ioffset, char **buf) const
 {
 	uint32_t offset;
 	
-	if (idxfd > 0) {
+    if (idxfd) {
 		idxfd->seek(ioffset, SEEK_SET);
 		idxfd->read(&offset, 4);
 
