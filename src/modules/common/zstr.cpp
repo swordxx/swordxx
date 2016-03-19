8a520e8e0dfaa05ad703edad623bbbd19ba0049a
Jaak Ristioja <jaak@ristioja.ee>
2016-03-19 11:15:52 +0200

    zStr: Fixed pointer checks.
    One of these was outright faulty.

--- src/modules/common/zstr.cpp
+++ src/modules/common/zstr.cpp
@@ -83,7 +83,7 @@ zStr::zStr(const char *ipath, int fileMode, long blockCount, SWCompress *icomp, 
 	buf.setFormatted("%s.zdt", path);
 	zdtfd = FileMgr::getSystemFileMgr()->open(buf, fileMode, true);
 
-	if (datfd <= 0) {
+    if (!datfd) {
 		SWLog::getSystemLog()->logError("%d", errno);
 	}
 
@@ -133,7 +133,7 @@ void zStr::getKeyFromDatOffset(long ioffset, char **buf) const
 {
 	int size;
 	char ch;
-	if (datfd > 0) {
+    if (datfd) {
 		datfd->seek(ioffset, SEEK_SET);
 		for (size = 0; datfd->read(&ch, 1) == 1; size++) {
 			if ((ch == '\\') || (ch == 10) || (ch == 13))
@@ -167,7 +167,7 @@ void zStr::getKeyFromIdxOffset(long ioffset, char **buf) const
 {
 	uint32_t offset;
 	
-	if (idxfd > 0) {
+    if (idxfd) {
 		idxfd->seek(ioffset, SEEK_SET);
 		idxfd->read(&offset, 4);
 		offset = swordtoarch32(offset);
