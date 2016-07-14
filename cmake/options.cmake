182a1c14f4e07be67bfae89e249e4495ae2c6f24
Jaak Ristioja <jaak@ristioja.ee>
2016-07-14 11:25:59 +0300

    cmake/options.cmake: Removed _SET_FANCY macro.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,33 +1,14 @@
-# Borrowed
-#
-# This macro implements some very special logic how to deal with the cache.
-# By default the various install locations inherit their value from their "parent" variable
-# so if you set CMAKE_INSTALL_PREFIX, then EXEC_INSTALL_PREFIX, PLUGIN_INSTALL_DIR will
-# calculate their value by appending subdirs to CMAKE_INSTALL_PREFIX .
-# This would work completely without using the cache.
-# But if somebody wants e.g. a different EXEC_INSTALL_PREFIX this value has to go into
-# the cache, otherwise it will be forgotten on the next cmake run.
-# Once a variable is in the cache, it doesn't depend on its "parent" variables
-# anymore and you can only change it by editing it directly.
-# this macro helps in this regard, because as long as you don't set one of the
-# variables explicitely to some location, it will always calculate its value from its
-# parents. So modifying CMAKE_INSTALL_PREFIX later on will have the desired effect.
-# But once you decide to set e.g. EXEC_INSTALL_PREFIX to some special location
-# this will go into the cache and it will no longer depend on CMAKE_INSTALL_PREFIX.
-macro(_SET_FANCY _var _value _comment)
-    if (NOT DEFINED ${_var})
-        set(${_var} ${_value})
-    else()
-        set(${_var} "${${_var}}" CACHE STRING "${_comment}")
-    endif()
-endmacro(_SET_FANCY)
+SET(LIB_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/lib"
+    CACHE STRING "Object code library install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/lib")
 
-_SET_FANCY(LIB_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/lib" "Object code library install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/lib")
+SET(INCLUDE_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/include"
+    CACHE STRING "C Header files install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/include.")
 
-_SET_FANCY(INCLUDE_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/include" "C Header files install directory. Defaults to ${CMAKE_INSTALL_PREFIX}/include.")
+SET(BINDIR "${CMAKE_INSTALL_PREFIX}/bin"
+    CACHE STRING "Directory to install binary executable files. Defaults to ${CMAKE_INSTALL_PREFIX}/bin.")
 
-SET(BINDIR "${CMAKE_INSTALL_PREFIX}/bin" CACHE STRING "Directory to install binary executable files. Defaults to ${CMAKE_INSTALL_PREFIX}/bin.")
+SET(SYSCONF_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/etc"
+    CACHE STRING "Directory to install global config files. Defaults to ${CMAKE_INSTALL_PREFIX}/etc.")
 
-_SET_FANCY(SYSCONF_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/etc" "Directory to install global config files. Defaults to ${CMAKE_INSTALL_PREFIX}/etc.")
-
-_SET_FANCY(SHARE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/share" "Directory to install global data files. Defaults to ${CMAKE_INSTALL_PREFIX}/share.")
+SET(SHARE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/share"
+    CACHE STRING "Directory to install global data files. Defaults to ${CMAKE_INSTALL_PREFIX}/share.")
