5ea3054d502da0e0e0776eb95e520eac5c97d805
Jaak Ristioja <jaak@ristioja.ee>
2016-09-23 23:39:07 +0300

    RemoteTransport: Some copyDirectory() refactoring. Many bugs still left.

--- src/swordxx/remotetrans.cpp
+++ src/swordxx/remotetrans.cpp
@@ -31,8 +31,6 @@
 #include "utilstr.h"
 
 
-using std::vector;
-
 namespace swordxx {
 
 RemoteTransport::RemoteTransport(const char *host, StatusReporter *statusReporter)
@@ -48,82 +46,90 @@ RemoteTransport::RemoteTransport(const char *host, StatusReporter *statusReporte
 RemoteTransport::~RemoteTransport() {
 }
 
-int RemoteTransport::copyDirectory(const char *urlPrefix, const char *dir, const char *dest, const char *suffix) {
+int RemoteTransport::copyDirectory(const char * urlPrefix_,
+                                   const char * dir_,
+                                   const char * dest_,
+                                   const char * suffix)
+{
     SWLog::getSystemLog()->logDebug("RemoteTransport::copyDirectory");
-    int retVal = 0;
+    std::string const url(
+                [&urlPrefix_, &dir_]() {
+                    std::string url(std::string(urlPrefix_ ? urlPrefix_ : "")
+                                    + std::string(dir_ ? dir_ : ""));
+                    removeTrailingDirectorySlashes(url);
+                    url += '/';
+                    return url;
+                }());
+    std::string const dest(
+                [&dest_](){
+                    std::string dest(dest_ ? dest_ : "");
+                    removeTrailingDirectorySlashes(dest);
+                    dest += "/";
+                    return dest;
+                }());
 
-    std::string url = std::string(urlPrefix ? urlPrefix : "") + std::string(dir ? dir : "");
-    removeTrailingDirectorySlashes(url);
-    url += '/';
+    SWLog::getSystemLog()->logDebug("NetTransport: getting dir %s\n",
+                                    url.c_str());
+    std::vector<DirEntry> dirList = getDirList(url.c_str());
 
-    SWLog::getSystemLog()->logDebug("NetTransport: getting dir %s\n", url.c_str());
-    vector<struct DirEntry> dirList = getDirList(url.c_str());
-
-    if (!dirList.size()) {
-        SWLog::getSystemLog()->logWarning("NetTransport: failed to read dir %s\n", url.c_str());
+    if (dirList.empty()) {
+        SWLog::getSystemLog()->logWarning(
+                    "NetTransport: failed to read dir %s\n",
+                    url.c_str());
         return -1;
     }
 
     // append files in sub directories and calculate total download size
-    unsigned int i = 0;
-    long totalBytes = 0;
-    for (;;) {
-        if (i == dirList.size())
-            break;
-
-        struct DirEntry &e = dirList.at(i);
-
+    std::size_t totalBytes = 0u;
+    for (std::size_t i = 0u; i != dirList.size(); ) {
+        DirEntry const & e = dirList[i];
         if (e.isDirectory) {
-            std::string name(e.name); // &e will be invalidated after first insertion
-            vector<struct DirEntry> sd = getDirList((url + name + '/').c_str());
-            for (unsigned int ii = 0; ii < sd.size(); ii++) {
-                sd[ii].name = name + '/' + sd[ii].name;
-                dirList.push_back(sd[ii]);
-            }
+            std::string const name(e.name + '/');
             dirList.erase(dirList.begin() + i);
-        }
-        else {
+            for (auto & dirEntry : getDirList((url + name).c_str())) {
+                dirEntry.name = name + dirEntry.name;
+                dirList.push_back(dirEntry);
+            }
+        } else {
             totalBytes += e.size;
             i++;
         }
     }
 
-    long completedBytes = 0;
-    for (i = 0; i < dirList.size(); i++) {
-        struct DirEntry &dirEntry = dirList[i];
-        std::string buffer(dest ? dest : "");
-        removeTrailingDirectorySlashes(buffer);
-        buffer += "/";
-        buffer += dirEntry.name;
-        if (!strcmp(&buffer.c_str()[buffer.length()-strlen(suffix)], suffix)) {
-            std::string buffer2 = "Downloading (";
-            buffer2 += formatted("%d", i+1);
-            buffer2 += " of ";
-            buffer2 += formatted("%d", dirList.size());
-            buffer2 += "): ";
-            buffer2 += dirEntry.name;
-            if (statusReporter)
-                statusReporter->preStatus(totalBytes, completedBytes, buffer2.c_str());
-            FileMgr::createParent(buffer.c_str());	// make sure parent directory exists
+    std::size_t completedBytes = 0u;
+    auto const suffixLength(std::strlen(suffix));
+    for (std::size_t i = 0u; i < dirList.size(); ++i) {
+        DirEntry const & dirEntry = dirList[i];
+        std::string const buffer(dest + dirEntry.name);
+        if (!std::strcmp(&buffer.c_str()[buffer.length() - suffixLength], suffix)) {
+            if (statusReporter) {
+                std::string buffer2 = "Downloading (";
+                buffer2 += formatted("%d", i+1);
+                buffer2 += " of ";
+                buffer2 += formatted("%d", dirList.size());
+                buffer2 += "): ";
+                buffer2 += dirEntry.name;
+                statusReporter->preStatus(totalBytes,
+                                          completedBytes,
+                                          buffer2.c_str());
+            }
+            // make sure parent directory exists:
+            FileMgr::createParent(buffer.c_str());
             try {
-                std::string url = (std::string)urlPrefix + (std::string)dir;
-                removeTrailingDirectorySlashes(url);
-                url += "/";
-                url += dirEntry.name;
-                if (!getUrl(buffer.c_str(), url.c_str())) {
-                    SWLog::getSystemLog()->logWarning("copyDirectory: failed to get file %s\n", url.c_str());
+                std::string const url2(url + dirEntry.name);
+                if (!getUrl(buffer.c_str(), url2.c_str())) {
+                    SWLog::getSystemLog()->logWarning(
+                                "copyDirectory: failed to get file %s\n",
+                                url2.c_str());
                     return -2;
                 }
                 completedBytes += dirEntry.size;
-            }
-            catch (...) {}
-            if (term.load(std::memory_order_acquire)) {
-                retVal = -3;
-                break;
-            }
+            } catch (...) {}
+            if (term.load(std::memory_order_acquire))
+                return -3;
         }
     }
-    return retVal;
+    return 0;
 }
 
 } /* namespace swordxx */
