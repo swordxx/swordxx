44e4861a372916aa394442d3ab60aba18d8f9d78
Gregory Hellings <ghelling@redhat.com>
2016-09-26 13:57:38 -0400

    Added travis file
    Need to pre-install packages
    Added CLucene to Travis
    Add gcc 5 to Travis
    Change language type to avoid exports
    Expand matrix to include two CMake versions
    Moved Travis script to external script for lint

---
+++ travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This is a minimal script to build the library within the TravisCI
+# tests. It is not meant to be demonstrative, used by end users, or
+# any other set of such consumption. Although you can choose to take
+# it as informative if you wish
+mkdir -p build
+cd build
+/opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake .. || exit 1
+make || exit 1
