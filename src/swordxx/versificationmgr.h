fc1654f9b793bfbed9b3351dc95868070468c866
Jaak Ristioja <jaak@ristioja.ee>
2016-10-25 18:09:04 +0300

    VersificationMgr: Fixed type error in Book::getChapterMax().

--- src/swordxx/versificationmgr.h
+++ src/swordxx/versificationmgr.h
@@ -123,7 +123,7 @@ public:
         const char *getLongName() const noexcept { return m_longName.c_str(); }
         const char *getOSISName() const noexcept { return m_osisName.c_str(); }
         const char *getPreferredAbbreviation() const noexcept { return m_prefAbbrev.c_str(); }
-        int getChapterMax() const noexcept { return m_chapMax; }
+        unsigned int getChapterMax() const noexcept { return m_chapMax; }
         int getVerseMax(int chapter) const;
     };
 
