swordxx/swordxx

commit e6ee6146bba4533e2c065699a2ea990f5369d32e

SWMgr: Small addGlobalOptions() optimizations.

src/swordxx/swmgr.cpp
@@ -33,6 +33,7 @@
 #endif
 #include <iterator>
 #include <sys/stat.h>
+#include <tuple>
 #include "filemgr.h"
 #include "filters/cipherfil.h"
 #include "filters/gbffootnotes.h"
@@ -928,9 +929,9 @@ void SWMgr::addGlobalOptions(SWModule & module,
 {
     for (; start != end; ++start) {
         std::string filterName(start->second);
+        decltype(optionFilters)::const_iterator it;
 
         // special cases for filters with parameters
-
         if (hasPrefix(filterName, "OSISReferenceLinks")) {
             std::string params = filterName;
             filterName = stripPrefix(params, '|');
@@ -949,23 +950,22 @@ void SWMgr::addGlobalOptions(SWModule & module,
                                 optionType.c_str(),
                                 optionSubType.c_str(),
                                 optionDefaultValue.c_str()));
-                optionFilters.emplace(filterName, filter.get());
+                std::tie(it, std::ignore) =
+                        optionFilters.emplace(filterName, filter.get());
                 cleanupFilters.emplace_back(std::move(filter));
             }
+        } else {
+            it = optionFilters.find(filterName);
+            if (it == optionFilters.end())
+                continue;
         }
 
-
-        decltype(optionFilters)::const_iterator const it(
-                    optionFilters.find(filterName));
-        if (it != optionFilters.end()) {
-            // Add filter to module and option as a valid option:
-            module.addOptionFilter(it->second);
-            for (auto const & option : options)
-                if (!strcmp(option.c_str(), it->second->getOptionName()))
-                    goto option_found;
+        // Add filter to module and option as a valid option:
+        module.addOptionFilter(it->second);
+        if (std::find(options.cbegin(),
+                      options.cend(),
+                      it->second->getOptionName()) == options.cend())
             options.push_back(it->second->getOptionName());
-            option_found: (void) options;
-        }
     }
 
     if (filterMgr)

commit b9ccb9a35361286c783398016b551a5af35c0f87

SWMgr: Fixed bug introduced in previous commit (e6ee6146).

src/swordxx/swmgr.cpp
@@ -943,7 +943,8 @@ void SWMgr::addGlobalOptions(SWModule & module,
             // we'll key off of type and subtype.
             filterName = filterName + "." + optionType + "." + optionSubType;
 
-            if (optionFilters.find(filterName) == optionFilters.end()) {
+            it = optionFilters.find(filterName);
+            if (it == optionFilters.end()) {
                 auto filter(std::make_unique<OSISReferenceLinks>(
                                 optionName.c_str(),
                                 optionTip.c_str(),