45ef07395b29f2718f9e460449a3f35cd217b77b
Jaak Ristioja <jaak@ristioja.ee>
2017-01-08 21:38:01 +0200

    SWCompress: Improved memory safety.

--- src/swordxx/modules/common/swcomprs.cpp
+++ src/swordxx/modules/common/swcomprs.cpp
@@ -28,6 +28,24 @@
 
 
 namespace swordxx {
+namespace {
+
+char * saferMemoryOperationWrap(void * const ptr) {
+    if (ptr)
+        return static_cast<char *>(ptr);
+    throw std::bad_alloc();
+};
+
+char * saferMalloc(std::size_t const size)
+{ return saferMemoryOperationWrap(std::malloc(size)); };
+
+char * saferRealloc(char * const ptr, std::size_t const size)
+{ return saferMemoryOperationWrap(std::realloc(ptr, size)); };
+
+char * saferCalloc(std::size_t const nmemb, std::size_t const size)
+{ return saferMemoryOperationWrap(std::calloc(nmemb, size)); };
+
+}
 
 SWCompress::SWCompress() {}
 
@@ -51,16 +69,17 @@ void SWCompress::reset() {
 char * SWCompress::Buf(const char * ibuf, unsigned long * len) {
     // setting an uncompressed buffer
     if (ibuf) {
+        auto const newBuffer = saferCalloc(slen + 1, 1);
         reset();
         slen = (len) ? *len : std::strlen(ibuf);
-        buf = static_cast<char *>(std::calloc(slen + 1, 1));
+        buf = newBuffer;
         std::memcpy(buf, ibuf, slen);
     }
 
     // getting an uncompressed buffer
     if (!buf) {
         // be sure we at least allocate an empty buf for return:
-        buf = static_cast<char *>(std::calloc(1, 1));
+        buf = saferCalloc(1, 1);
         direct = 1;
         Decode();
 //        slen = strlen(buf);
@@ -73,8 +92,9 @@ char * SWCompress::Buf(const char * ibuf, unsigned long * len) {
 char * SWCompress::zBuf(unsigned long * len, char * ibuf) {
     // setting a compressed buffer
     if (ibuf) {
+        auto const newBuffer = saferMalloc(*len);
         reset();
-        zbuf = static_cast<char *>(std::malloc(*len));
+        zbuf = newBuffer;
         std::memcpy(zbuf, ibuf, *len);
         zlen = *len;
     }
@@ -110,15 +130,6 @@ unsigned long SWCompress::GetChars(char * ibuf, unsigned long len) {
 
 /// \note override for other than buffer compression
 unsigned long SWCompress::SendChars(char *ibuf, unsigned long len) {
-    /** \bug Just avoids a null dereference by throwing std::bad_alloc, which is
-             not properly handled. */
-    static auto const saferRealloc =
-            [](void * const ptr, std::size_t const size) {
-                if (auto * const r = std::realloc(ptr, size))
-                    return static_cast<char *>(r);
-                throw std::bad_alloc();
-            };
-
     if (direct) {
         if (buf) {
 //            slen = strlen(buf);
@@ -127,7 +138,7 @@ unsigned long SWCompress::SendChars(char *ibuf, unsigned long len) {
                 std::memset(&buf[pos], 0, len + 1024);
             }
         } else {
-            buf = static_cast<char *>(std::calloc(1, len + 1024));
+            buf = saferCalloc(1, len + 1024);
         }
         std::memmove(&buf[pos], ibuf, len);
         pos += len;
@@ -138,7 +149,7 @@ unsigned long SWCompress::SendChars(char *ibuf, unsigned long len) {
                 zlen = zpos + len + 1024;
             }
         } else {
-            zbuf = static_cast<char *>(std::calloc(1, len + 1024));
+            zbuf = saferCalloc(1, len + 1024);
             zlen = len + 1024;
         }
         std::memmove(&zbuf[zpos], ibuf, len);
