f8521a91918f82db07f24fb21c4914378bc645fe
Jaak Ristioja <jaak@ristioja.ee>
2017-01-10 10:52:48 +0200

    travis.sh: Also run `make tests`

--- travis.sh
+++ travis.sh
@@ -8,4 +8,5 @@ mkdir -p build
 cd build
 /opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=install/ .. || exit 1
 make || exit 1
+make tests || exit 1
 make install || exit 1
