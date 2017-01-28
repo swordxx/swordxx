2674cd562aee3ab3b4f6a710159344c25186bc85
Jaak Ristioja <jaak@ristioja.ee>
2017-01-29 00:10:22 +0200

    tests, xmltest: Some refactoring.

--- tests/xmltest.cpp
+++ tests/xmltest.cpp
@@ -22,54 +22,47 @@
 
 #include <iostream>
 #include <swordxx/utilxml.h>
+#include <type_traits>
 
 
-using namespace swordxx;
-using namespace std;
+int main(int argc, char * argv[]) {
+    static char const xml[] =
+            "<verse osisID=\"John.1.1\" type=\'test type\' yeah = \"stuff\" />";
+    std::cout << ((argc > 1) ? argv[1]: xml) << "\n";
 
-int main(int argc, char **argv) {
+    swordxx::XMLTag x(argc > 1 ? argv[1] : xml);
 
-    const char *xml = "<verse osisID=\"John.1.1\" type=\'test type\' yeah = \"stuff\" />";
-    cout << ((argc > 1) ? argv[1]: xml) << "\n";
-
-    XMLTag x((argc > 1) ? argv[1] : xml);
-
-    cout << x.toString() << "\n";
+    std::cout << x.toString() << "\n";
     x.setAttribute("addedAttribute", "with a \" quote");
-    cout << x.toString() << "\n";
-    cout << "Tag name: [" << x.getName() << "]\n";
-    StringList attributes = x.getAttributeNames();
-    for (StringList::iterator it = attributes.begin(); it != attributes.end(); it++) {
-        const char *name = it->c_str();
-        cout << " - attribute: [" << name << "] = [";
-        cout << x.getAttribute(name) << "]\n";
-        int count = x.getAttributePartCount(name, ' ');
-        cout << "\t" << count << " parts:\n";
-        int i = (count > 1) ? 0 : -1;        // -1 for whole value cuz it's faster, but does the same thing as 0
+    std::cout << x.toString() << "\nTag name: [" << x.getName() << "]\n";
+    for (auto const & name : x.getAttributeNames()) {
+        auto const count = x.getAttributePartCount(name.c_str(), ' ');
+        std::cout << " - attribute: [" << name << "] = ["
+                  << x.getAttribute(name.c_str()) << "]\n\t" << count
+                  << " parts:\n";
+        // -1 for whole value cuz it's faster, but does the same thing as 0:
+        std::remove_cv_t<decltype(count)> i = (count > 1) ? 0 : -1;
         do {
-            cout << "\t" << x.getAttribute(name, i, ' ') << "\n";
-            if (i < 0) i = 0;    // to handle our -1 condition
+            std::cout << "\t" << x.getAttribute(name.c_str(), i, ' ') << "\n";
+            if (i < 0)
+                i = 0;    // to handle our -1 condition
         } while (++i < count);
     }
-    cout << " isEmpty: " << x.isEmpty() << "\n";
-    cout << " isEndTag: " << x.isEndTag() << "\n";
-    cout << "\n";
-
+    std::cout << " isEmpty: " << x.isEmpty() << "\n isEndTag: " << x.isEndTag()
+              << "\n\n";
 
     if (argc < 2) {  // only run if we're defaulted
-        const char *testParts="ABC D EF GHIJ";
-        cout << "Setting attribute 'multiPart' to: '" << testParts << "'\n";
+        static char const testParts[] = "ABC D EF GHIJ";
+        std::cout << "Setting attribute 'multiPart' to: '" << testParts
+                  << "'\n";
         x.setAttribute("multiPart", testParts);
-        cout << x.toString() << "\n";
-        cout << "Setting part 2 to 'MMM'\n";
+        std::cout << x.toString() << "\nSetting part 2 to 'MMM'\n";
         x.setAttribute("multiPart", "MMM", 2, ' ');
-        cout << x.toString() << "\n";
-        cout << "Removing part 1\n";
+        std::cout << x.toString() << "\nRemoving part 1\n";
         x.setAttribute("multiPart", nullptr, 1, ' ');
-        cout << x.toString() << "\n";
-        cout << "Removing part 2\n";
+        std::cout << x.toString() << "\nRemoving part 2\n";
         x.setAttribute("multiPart", nullptr, 2, ' ');
-        cout << x.toString() << "\n";
+        std::cout << x.toString() << std::endl;
     }
 
 }
