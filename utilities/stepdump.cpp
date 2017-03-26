8143c2b0637e851eb1b2ce90c2042deaeb44437d
Jaak Ristioja <jaak@ristioja.ee>
2017-03-27 00:39:56 +0300

    stepdump: Improved memory safety.

--- utilities/stepdump.cpp
+++ utilities/stepdump.cpp
@@ -32,6 +32,7 @@
 #include <io.h>
 #endif
 #include <iostream>
+#include <memory>
 #include <string>
 #include <swordxx/filemgr.h>
 #include <swordxx/modules/common/lzsscomprs.h>
@@ -75,17 +76,16 @@ typedef struct {
 } ViewableBlock;
 
 void readVersion(int fd, Version *versionRecord);
-void readHeaderControlWordAreaText(int fd, char **buf);
+void readHeaderControlWordAreaText(int fd);
 void readViewableHeader(int fd, ViewableHeader *viewableHeaderRecord);
 void readViewableBlock(int fd, ViewableBlock *vb);
-void readViewableBlockText(int fd, ViewableBlock *vb, char **buf);
+void readViewableBlockText(int fd, ViewableBlock *vb);
 
 SWCompress * compress = nullptr;
 
 int main(int argc, char **argv) {
 
     compress = new LZSSCompress();
-    char *buf;
     Version versionRecord;
     ViewableHeader viewableHeaderRecord;
 
@@ -111,14 +111,12 @@ int main(int argc, char **argv) {
 
     try {
         readVersion(fd, &versionRecord);
-        readHeaderControlWordAreaText(fd, &buf);
+        readHeaderControlWordAreaText(fd);
     }
     catch (std::runtime_error &e) {
         std::cout << e.what () << '\n';
         exit (-3);
     }
-    delete [] buf;
-    buf = nullptr;
 
 
     fileName = bookpath + "Viewable.idx";
@@ -144,14 +142,12 @@ int main(int argc, char **argv) {
 
     try {
         readViewableBlock(fdv, &vb);
-        readViewableBlockText(fd, &vb, &buf);
+        readViewableBlockText(fd, &vb);
     }
     catch (std::runtime_error &e) {
         std::cout << e.what () << '\n';
         exit (-6);
     }
-    delete [] buf;
-    buf = nullptr;
 
     int nonGlossBlocksCount = viewableHeaderRecord.viewableBlocksCount
                         - viewableHeaderRecord.glossBlocksCount;
@@ -161,18 +157,14 @@ int main(int argc, char **argv) {
     for (int i = 1; i < nonGlossBlocksCount; i++) {
         cout << "\nNon-Glossary viewable block: " << i;
         readViewableBlock(fdv, &vb);
-        readViewableBlockText(fd, &vb, &buf);
-        delete [] buf;
-        buf = nullptr;
+        readViewableBlockText(fd, &vb);
     }
 
     cout << "\n\nReading " << viewableHeaderRecord.glossBlocksCount << " glossary viewable blocks";
     for (int i = 0; i < viewableHeaderRecord.glossBlocksCount; i++) {
         cout << "\nGlossary viewable block: " << i;
         readViewableBlock(fdv, &vb);
-        readViewableBlockText(fd, &vb, &buf);
-        delete [] buf;
-        buf = nullptr;
+        readViewableBlockText(fd, &vb);
     }
 
     close(fdv);
@@ -185,6 +177,7 @@ int main(int argc, char **argv) {
 void readVersion(int fd, Version *versionRecord) {
 
     string readError = "Error reading Version Record.";
+    string seekError = "Error seeking Version Record.";
 
     cout << "\n\nReading Version Record (" << 16/*sizeof(struct Version)*/ << " bytes)\n\n";
 // DO NOT USE BECAUSE OF RECORD BYTE ALIGNMENT PROBLEMS
@@ -229,10 +222,8 @@ void readVersion(int fd, Version *versionRecord) {
 
     if (skip) {
         cout << "\nSkipping " << skip << " unknown bytes.\n";
-        char *skipbuf = new char[skip];
-        rtn = read(fd, skipbuf, skip);
-        delete [] skipbuf;
-        if (rtn <= 0) throw runtime_error(readError);
+        if (lseek(fd, skip, SEEK_CUR) < 0)
+            throw runtime_error(seekError);
     }
 }
 
@@ -240,6 +231,7 @@ void readVersion(int fd, Version *versionRecord) {
 void readViewableHeader(int fd, ViewableHeader *viewableHeaderRecord) {
 
     string readError = "Error reading Viewable Header.";
+    string seekError = "Error seeking Viewable Header.";
 
     cout << "\n\nReading Viewable Header Record (" << 16/*sizeof(struct ViewableHeader)*/ << " bytes)\n\n";
 
@@ -273,28 +265,27 @@ void readViewableHeader(int fd, ViewableHeader *viewableHeaderRecord) {
 
     if (skip) {
         cout << "\nSkipping " << skip << " unknown bytes.\n";
-        char *skipbuf = new char[skip];
-        rtn = read(fd, skipbuf, skip);
-        delete [] skipbuf;
-        if (rtn <= 0) throw runtime_error(readError);
+        if (lseek(fd, skip, SEEK_CUR) < 0)
+            throw runtime_error(seekError);
     }
 }
 
 
-void readViewableBlockText(int fd, ViewableBlock *vb, char **buf) {
+void readViewableBlockText(int fd, ViewableBlock * vb) {
     string readError = "Error reading Viewable Block Text.";
 
     unsigned long size = vb->size;
 
-    *buf = new char [ ((vb->size > vb->uncompressedSize) ? vb->size : vb->uncompressedSize) + 1 ];
+    auto buf(std::make_unique<char[]>(((vb->size > vb->uncompressedSize)
+                                       ? vb->size
+                                       : vb->uncompressedSize) + 1));
     lseek(fd, vb->offset, SEEK_SET);
-    int rtn = read(fd, *buf, vb->size);
+    int rtn = read(fd, buf.get(), vb->size);
     if (rtn <= 0) throw runtime_error(readError);
 
-    compress->zBuf(&size, *buf);
-    strcpy(*buf, compress->Buf());
-    cout << "Viewable Block Text:\n";
-    cout << *buf << "\n\n";
+    compress->zBuf(&size, buf.get());
+    strcpy(buf.get(), compress->Buf());
+    cout << "Viewable Block Text:\n" << buf.get() << "\n\n";
 }
 
 
@@ -319,7 +310,7 @@ void readViewableBlock(int fd, ViewableBlock *vb) {
 }
 
 
-void readHeaderControlWordAreaText(int fd, char **buf) {
+void readHeaderControlWordAreaText(int fd) {
     string readError = "Error reading Control Word Area.";
 
     long headerControlWordAreaSize;
@@ -327,11 +318,11 @@ void readHeaderControlWordAreaText(int fd, char **buf) {
     if (rtn <= 0) throw runtime_error(readError);
     cout << "Reading Header Control Word Area (" << headerControlWordAreaSize << " bytes)\n\n";
 
-    *buf = new char [headerControlWordAreaSize + 1];
+    auto const buf(std::make_unique<char[]>(headerControlWordAreaSize + 1));
 
-    rtn = read(fd, *buf, headerControlWordAreaSize);
+    rtn = read(fd, buf.get(), headerControlWordAreaSize);
     if (rtn <= 0) throw runtime_error(readError);
-    (*buf)[headerControlWordAreaSize] = 0;
+    buf[headerControlWordAreaSize] = 0;
 
-    cout << "headerControlWordArea:\n" << *buf << "\n";
+    cout << "headerControlWordArea:\n" << buf.get() << "\n";
 }
