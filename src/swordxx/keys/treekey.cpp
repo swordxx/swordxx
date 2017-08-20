swordxx/swordxx

commit 8a2f5b492e884a7b8a0a4bb4bc4b04d79b1e63ec

TreeKey: Memory safety changes in assureKeyPath.

src/swordxx/keys/treekey.cpp
@@ -36,19 +36,15 @@ void TreeKey::init() {
 
 
 void TreeKey::assureKeyPath(const char *keyBuffer) {
-
-    if (!keyBuffer)
-        keyBuffer = m_unsnappedKeyText.c_str();
-
-    char * keybuf = nullptr;
-    stdstr(&keybuf, keyBuffer);
+    std::string keyBuf(keyBuffer ? std::string(keyBuffer) : m_unsnappedKeyText);
 
     root();
 
-    // TODO: change to NOT use strtok. strtok is dangerous.
-    std::string tok = strtok(keybuf, "/");
-    trimString(tok);
-    while (!tok.empty()) {
+    std::string tok(stripPrefix(keyBuf, '/'));
+    for (;; tok = stripPrefix(keyBuf, '/')) {
+        trimString(tok);
+        if (tok.empty())
+            break;
         bool foundkey = false;
         if (hasChildren()) {
             firstChild();
@@ -79,12 +75,7 @@ void TreeKey::assureKeyPath(const char *keyBuffer) {
 #ifdef DEBUG
 //      std::cout << getLocalName() << " : " << tok << std::endl;
 #endif
-
-        tok = strtok(nullptr, "/");
-        trimString(tok);
-
     }
-    delete [] keybuf;
 }
 
 

commit 27c406ef0b46cbb867a6864296623de988e23a35

TreeKey: Fixed bug in assureKeyPath() introduced by 8a2f5b49.

src/swordxx/keys/treekey.cpp
@@ -40,8 +40,19 @@ void TreeKey::assureKeyPath(const char *keyBuffer) {
 
     root();
 
-    std::string tok(stripPrefix(keyBuf, '/'));
-    for (;; tok = stripPrefix(keyBuf, '/')) {
+    auto const getNextNonEmptyToken = // strtok
+            [](std::string & str) {
+                for (;;) {
+                    auto r(stripPrefix(str, '/'));
+                    if (!r.empty())
+                        return r;
+                    if (str.empty())
+                        return r;
+                }
+            };
+
+    std::string tok(getNextNonEmptyToken(keyBuf));
+    for (;; tok = getNextNonEmptyToken(keyBuf)) {
         trimString(tok);
         if (tok.empty())
             break;