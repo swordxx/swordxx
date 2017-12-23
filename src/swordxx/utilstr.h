2f7098f4a542423d4417d2736333030cc120ca56
Jaak Ristioja <jaak@ristioja.ee>
2017-12-24 00:43:22 +0200

    utilstr.h: Implemented charIs(Upper|Lower|Alpha|Alnum) constexpr functions.

--- src/swordxx/utilstr.h
+++ src/swordxx/utilstr.h
@@ -48,6 +48,76 @@ constexpr bool charIsDigit(char const c) noexcept {
     }
 }
 
+constexpr bool charIsUpper(char const c) noexcept {
+    switch (c) {
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y':
+        case 'Z':
+            return true;
+        default:
+            return false;
+    }
+}
+
+constexpr bool charIsLower(char const c) noexcept {
+    switch (c) {
+        case 'a': case 'b': case 'c': case 'd': case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y':
+        case 'z':
+            return true;
+        default:
+            return false;
+    }
+}
+
+constexpr bool charIsAlpha(char const c) noexcept {
+    switch (c) {
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y':
+        case 'Z':
+        case 'a': case 'b': case 'c': case 'd': case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y':
+        case 'z':
+            return true;
+        default:
+            return false;
+    }
+}
+
+constexpr bool charIsAlnum(char const c) noexcept {
+    switch (c) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y':
+        case 'Z':
+        case 'a': case 'b': case 'c': case 'd': case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y':
+        case 'z':
+            return true;
+        default:
+            return false;
+    }
+}
+
 constexpr bool charIsSpace(char const c) noexcept {
     switch (c) {
         case ' ': case '\n': case '\r': case '\t': return true;
