933ee063c82574651dff9d206a58cefa29fb53fd
Jaak Ristioja <jaak@ristioja.ee>
2019-02-10 23:35:03 +0200

    EntriesBlock: Removed some C-style casts.

--- src/swordxx/modules/common/entriesblk.cpp
+++ src/swordxx/modules/common/entriesblk.cpp
@@ -37,17 +37,17 @@ const int EntriesBlock::METAENTRYSIZE = 8;
 
 EntriesBlock::EntriesBlock(const char *iBlock, unsigned long size) {
     if (size) {
-        block = (char *)std::calloc(1, size);
+        block = static_cast<char *>(std::calloc(1, size));
         std::memcpy(block, iBlock, size);
     }
     else {
-        block = (char *)std::calloc(1, sizeof(uint32_t));
+        block = static_cast<char *>(std::calloc(1, sizeof(uint32_t)));
     }
 }
 
 
 EntriesBlock::EntriesBlock() {
-    block = (char *)std::calloc(1, sizeof(uint32_t));
+    block = static_cast<char *>(std::calloc(1, sizeof(uint32_t)));
 }
 
 
