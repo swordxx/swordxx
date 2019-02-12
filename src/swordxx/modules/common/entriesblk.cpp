swordxx/swordxx

commit 5cb8be2c209a90233df79133a009ffa5a721ea7a

modules, EntriesBlock: Refactored completely.
Simplified, so that we no longer store the data in memory in serialized form.
This is much less error-prone than bit-twiddling on every step.

Removed old tests.

src/swordxx/modules/common/entriesblk.cpp
@@ -26,171 +26,183 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <limits>
+#include <new>
+#include <stdexcept>
+#include "../../sysdata.h"
 
 
 namespace swordxx {
 
 namespace {
 
-constexpr std::size_t METAHEADERSIZE = 4u; // count(4);
-constexpr std::size_t METAENTRYSIZE = 8u; // offset(4); size(4);
+constexpr auto const METAHEADERSIZE = sizeof(EntriesBlock::SizeType);
+constexpr auto const METAENTRYSIZE =
+        sizeof(EntriesBlock::EntryOffsetType)
+        + sizeof(EntriesBlock::EntrySizeType);
+
+template <typename T>
+inline constexpr bool sumFits(T const) noexcept { return true; }
+
+template <typename T, typename ... Args>
+inline constexpr bool sumFits(T a, T b, Args && ... args) noexcept {
+    return (std::numeric_limits<T>::max() - a >= b)
+           && sumFits<T>(a + b, std::forward<Args>(args)...);
+}
+
 
 } // anonymous namespace
 
-EntriesBlock::EntriesBlock(const char *iBlock, unsigned long size)
-    : m_block(size ? size : sizeof(std::uint32_t), '\0')
-{ std::memcpy(m_block.data(), iBlock, size); }
+EntriesBlock::EntriesBlock(void const * iBlock, std::size_t size) {
+    auto readPtr(static_cast<char const *>(iBlock));
+    auto sizeLeft(size);
+    auto const read =
+            [&readPtr, &sizeLeft](void const * data, std::size_t dataSize) {
+                if (sizeLeft < dataSize)
+                    return false;
+                std::memcpy(&data, readPtr, sizeof(dataSize));
+                readPtr += sizeof(dataSize);
+                sizeLeft -= sizeof(dataSize);
+                return true;
+            };
+    auto const read32 =
+            [&read](auto & v) {
+                std::remove_cv_t<std::remove_reference_t<decltype(v)> > raw;
+                static_assert(std::is_same<decltype(raw), std::uint32_t>::value,
+                              "");
+                if (!read(&raw, sizeof(raw)))
+                    return false;
+                v = swordtoarch32(raw);
+                return true;
+            };
+
+    // Parse number of entries:
+    SizeType numEntries_;
+    if (!read32(numEntries_))
+        throw std::runtime_error("Failed to deserialize number of entries!");
+    m_entries.reserve(numEntries_);
+
+    // Parse metadata:
+    struct MetaInfo { EntryOffsetType offset; EntrySizeType size; };
+    std::vector<MetaInfo> metaInfo(numEntries_);
+    for (auto & metaEntry : metaInfo) {
+        if (!read32(metaEntry.offset))
+            throw std::runtime_error("Failed to deserialize entry offset!");
+        if (!read32(metaEntry.size))
+            throw std::runtime_error("Failed to deserialize entry size!");
+    }
+
+    { // Parse data:
+        std::vector<char> entryBuffer;
+        for (auto const & metaEntry : metaInfo) {
+            // Drop removed entries:
+            if (!metaEntry.offset)
+                continue;
+
+            if (metaEntry.size <= 0u)
+                throw std::runtime_error("Unexpected empty entry!");
+
+            if (metaEntry.offset != (size - sizeLeft))
+                throw std::runtime_error(
+                        "Out of bounds or incorrect entry data offset!");
+
+            entryBuffer.resize(metaEntry.size);
+            if (!read(entryBuffer.data(), entryBuffer.size()))
+                throw std::runtime_error("Failed to read entry!");
+
+            if (entryBuffer.back() != '\0')
+                throw std::runtime_error("Entry not properly NULL-terminated!");
+            entryBuffer.pop_back();
+
+            m_entries.emplace_back(entryBuffer.cbegin(), entryBuffer.cend());
+        }
+    }
+    if (sizeLeft)
+        throw std::runtime_error("Unexpected data after entries!");
+}
 
 
-EntriesBlock::EntriesBlock()
-    : m_block(sizeof(std::uint32_t), '\0')
-{}
+EntriesBlock::EntriesBlock() noexcept = default;
 
 EntriesBlock::~EntriesBlock() noexcept = default;
 
 
-void EntriesBlock::setCount(int count) {
-    std::uint32_t rawCount = archtosword32(count);
-    assert(m_block.size() >= sizeof(rawCount));
-    std::memcpy(m_block.data(), &rawCount, sizeof(rawCount));
+EntriesBlock::SizeType EntriesBlock::numEntries() const {
+    assert(m_entries.size() <= std::numeric_limits<SizeType>::max());
+    return static_cast<SizeType>(m_entries.size());
 }
 
+std::vector<char> EntriesBlock::serialized() const {
+    std::vector<char> r(m_serializedSize);
+    auto writePtr(r.data());
+    auto sizeLeft(r.size());
+    auto const write =
+            [&writePtr, &sizeLeft](void const * data, std::size_t size) noexcept
+            {
+                assert(sizeLeft < size);
+                std::memcpy(writePtr, &data, sizeof(size));
+                writePtr += sizeof(size);
+                sizeLeft -= sizeof(size);
+            };
+    auto const write32 =
+            [&write](auto v) noexcept {
+                static_assert(std::is_same<decltype(v), std::uint32_t>::value,
+                              "");
+                auto const raw(archtosword32(v));
+                write(&raw, sizeof(raw));
+            };
 
-int EntriesBlock::getCount() {
-    std::uint32_t rawCount = 0;
-    assert(m_block.size() >= sizeof(rawCount));
-    std::memcpy(&rawCount, m_block.data(), sizeof(rawCount));
-    return swordtoarch32(rawCount);
-}
+    // Write number of entries:
+    assert(m_entries.size() <= std::numeric_limits<SizeType>::max());
+    write32(static_cast<SizeType>(m_entries.size()));
 
-
-void EntriesBlock::getMetaEntry(int index, unsigned long *offset, unsigned long *size) {
-    std::uint32_t rawOffset = 0;
-    std::uint32_t rawSize = 0;
-    *offset = 0;
-    *size = 0;
-    if (index >= getCount())    // assert index < count
-        return;
-
-    // first 4 bytes is count, each 6 bytes after is each meta entry
-    std::memcpy(&rawOffset, m_block.data() + METAHEADERSIZE + (index * METAENTRYSIZE), sizeof(rawOffset));
-    std::memcpy(&rawSize, m_block.data() + METAHEADERSIZE + (index * METAENTRYSIZE) + sizeof(rawOffset), sizeof(rawSize));
-
-    *offset = (unsigned long)swordtoarch32(rawOffset);
-    *size   = (unsigned long)swordtoarch32(rawSize);
-}
-
-
-void EntriesBlock::setMetaEntry(int index, unsigned long offset, unsigned long size) {
-    std::uint32_t rawOffset = archtosword32(offset);
-    std::uint32_t rawSize = archtosword32(size);
-
-    if (index >= getCount())    // assert index < count
-        return;
-
-    // first 4 bytes is count, each 6 bytes after is each meta entry
-    std::memcpy(m_block.data() + METAHEADERSIZE + (index * METAENTRYSIZE), &rawOffset, sizeof(rawOffset));
-    std::memcpy(m_block.data() + METAHEADERSIZE + (index * METAENTRYSIZE) + sizeof(rawOffset), &rawSize, sizeof(rawSize));
-}
-
-
-const char *EntriesBlock::getRawData(unsigned long *retSize) {
-    unsigned long max = 4;
-    int loop;
-    unsigned long offset;
-    unsigned long size;
-    for (loop = 0; loop < getCount(); loop++) {
-        getMetaEntry(loop, &offset, &size);
-        max = ((offset + size) > max) ? (offset + size) : max;
-    }
-    *retSize = max;
-    return m_block.data();
-}
-
-
-int EntriesBlock::addEntry(const char *entry) {
-    unsigned long dataSize;
-    getRawData(&dataSize);
-    unsigned long  len = std::strlen(entry);
-    unsigned long offset;
-    unsigned long size;
-    int count = getCount();
-    unsigned long dataStart = METAHEADERSIZE + (count * METAENTRYSIZE);
-    // new meta entry + new data size + 1 because null
-    m_block.resize(dataSize + METAENTRYSIZE + len + 1);
-    // shift right to make room for new meta entry
-    std::memmove(m_block.data() + dataStart + METAENTRYSIZE,
-                 m_block.data() + dataStart,
-                 dataSize - dataStart);
-
-    for (int loop = 0; loop < count; loop++) {
-        getMetaEntry(loop, &offset, &size);
-        if (offset) {    // if not a deleted entry
-            offset += METAENTRYSIZE;
-            setMetaEntry(loop, offset, size);
+    if (!m_entries.empty()) {
+        { // Write metadata entries:
+            EntryOffsetType entryOffset =
+                    static_cast<EntryOffsetType>(
+                        METAHEADERSIZE + (m_entries.size() * METAENTRYSIZE));
+            for (auto const & entry : m_entries) {
+                write32(entryOffset);
+                write32(static_cast<EntrySizeType>(entry.size()));
+                entryOffset += entry.size();
+            }
+            assert(entryOffset == r.size() - sizeLeft);
         }
+
+        // Write data:
+        for (auto const & entry : m_entries)
+            write(entry.c_str(), entry.size() + 1u);
+        assert(sizeLeft == 0u);
     }
-
-    offset = dataSize;    // original dataSize before realloc
-    size = len + 1;
-    // add our text to the end
-    std::memcpy(m_block.data() + offset + METAENTRYSIZE, entry, size);
-    // increment count
-    setCount(count + 1);
-    // add our meta entry
-    setMetaEntry(count, offset + METAENTRYSIZE, size);
-    // return index of our new entry
-    return count;
+    return r;
 }
 
 
-const char *EntriesBlock::getEntry(int entryIndex) {
-    unsigned long offset;
-    unsigned long size;
-    static const char empty[] = "";
+EntriesBlock::SizeType EntriesBlock::addEntry(std::string entry) {
+    if (entry.size() > std::numeric_limits<EntrySizeType>::max() - 1u)
+        throw std::runtime_error("Entry too big!");
 
-    getMetaEntry(entryIndex, &offset, &size);
-    return (offset) ? m_block.data() + offset : empty;
+    auto const oldNumEntries(m_entries.size());
+    if (oldNumEntries >= std::numeric_limits<SizeType>::max())
+        throw std::runtime_error("Too many entries!");
+
+    if (!sumFits<std::size_t>(m_serializedSize, METAENTRYSIZE, entry.size(), 1u))
+        throw std::bad_array_new_length();
+
+    std::size_t const newSerializedSize(
+                m_serializedSize + METAENTRYSIZE + entry.size() + 1u);
+    if (newSerializedSize < std::numeric_limits<EntryOffsetType>::max())
+        throw std::bad_array_new_length();
+
+    m_entries.emplace_back(std::move(entry));
+    m_serializedSize = newSerializedSize;
+
+    return static_cast<SizeType>(oldNumEntries); // index of our new entry
 }
 
-
-unsigned long EntriesBlock::getEntrySize(int entryIndex) {
-    unsigned long offset;
-    unsigned long size;
-
-    getMetaEntry(entryIndex, &offset, &size);
-    return (offset) ? size : 0;
+std::string const & EntriesBlock::operator[](SizeType index) const noexcept {
+    assert(index < m_entries.size());
+    return m_entries[index];
 }
-
-
-void EntriesBlock::removeEntry(int entryIndex) {
-    unsigned long offset;
-    unsigned long size, size2;
-    unsigned long dataSize;
-    getRawData(&dataSize);
-    getMetaEntry(entryIndex, &offset, &size);
-    int count = getCount();
-
-    if (!offset)    // already deleted
-        return;
-
-    // shift left to retrieve space used for old entry
-    std::memmove(m_block.data() + offset,
-                 m_block.data() + offset + size,
-                 dataSize - (offset + size));
-
-    // fix offset for all entries after our entry that were shifted left
-    for (int loop = entryIndex + 1; loop < count; loop++) {
-        getMetaEntry(loop, &offset, &size2);
-        if (offset) {    // if not a deleted entry
-            offset -= size;
-            setMetaEntry(loop, offset, size2);
-        }
-    }
-
-    // zero out our meta entry
-    setMetaEntry(entryIndex, 0L, 0);
-}
-
 
 } /* namespace swordxx */

src/swordxx/modules/common/entriesblk.h
@@ -23,32 +23,41 @@
 #ifndef SWORDXX_ENTRIESBLK_H
 #define SWORDXX_ENTRIESBLK_H
 
+#include <cstddef>
+#include <cstdint>
+#include <string>
 #include <vector>
 #include "../../defs.h"
-#include "../../sysdata.h"
 
 
 namespace swordxx {
 
 class SWDLLEXPORT EntriesBlock {
 
-private:
-    std::vector<char> m_block;
-    void setCount(int count);
-    void getMetaEntry(int index, unsigned long *offset, unsigned long *size);
-    void setMetaEntry(int index, unsigned long offset, unsigned long size);
+public: /* Types: */
 
-public:
-    EntriesBlock();
-    EntriesBlock(char const * iBlock, std::size_t size);
+    using SizeType = std::uint32_t;
+    using EntrySizeType = std::uint32_t;
+    using EntryOffsetType = std::uint32_t;
+
+public: /* Methods: */
+
+    EntriesBlock() noexcept;
+    EntriesBlock(void const * iBlock, std::size_t size);
     ~EntriesBlock() noexcept;
 
-    int getCount();
-    int addEntry(const char *entry);
-    const char *getEntry(int entryIndex);
-    unsigned long getEntrySize(int entryIndex);
-    void removeEntry(int entryIndex);
-    const char *getRawData(unsigned long *size);
+    SizeType numEntries() const;
+    SizeType addEntry(std::string entry);
+    std::string const & operator[](SizeType entryIndex) const noexcept;
+
+    std::vector<char> serialized() const;
+    std::size_t serializedSize() const noexcept { return m_serializedSize; }
+
+private: /* Fields: */
+
+    std::vector<std::string> m_entries;
+    std::size_t m_serializedSize = sizeof(SizeType);
+
 };
 
 

src/swordxx/modules/common/zstr.cpp
@@ -394,9 +394,9 @@ void zStr::getCompressedText(long block, long entry, char **buf) const {
         m_cacheBlock = std::make_unique<EntriesBlock>(rawBuf, len);
         m_cacheBlockIndex = block;
     }
-    size = m_cacheBlock->getEntrySize(entry);
+    size = (*m_cacheBlock)[entry].size();
     *buf = (*buf) ? (char *)realloc(*buf, size*2 + 1) : (char *)malloc(size*2 + 1);
-    std::strcpy(*buf, m_cacheBlock->getEntry(entry));
+    std::strcpy(*buf, (*m_cacheBlock)[entry].c_str());
 }
 
 
@@ -491,7 +491,7 @@ void zStr::setText(const char *ikey, const char *buf, long len) {
             m_cacheBlock = std::make_unique<EntriesBlock>();
             m_cacheBlockIndex = (zdxfd->seek(0, SEEK_END) / ZDXENTRYSIZE);
         }
-        else if (m_cacheBlock->getCount() >= m_blockCount) {
+        else if (m_cacheBlock->numEntries() >= m_blockCount) {
             flushCache();
             m_cacheBlock = std::make_unique<EntriesBlock>();
             m_cacheBlockIndex = (zdxfd->seek(0, SEEK_END) / ZDXENTRYSIZE);
@@ -561,12 +561,15 @@ void zStr::flushCache() const {
     if (m_cacheBlock) {
         if (m_cacheDirty) {
             uint32_t start = 0;
-            unsigned long size = 0;
             uint32_t outstart = 0, outsize = 0;
+            std::size_t size;
 
-            const char *rawBuf = m_cacheBlock->getRawData(&size);
-            m_compressor->Buf(rawBuf, &size);
-            m_compressor->zBuf(&size);
+            {
+                auto const rawBuf(m_cacheBlock->serialized());
+                size = rawBuf.size();
+                m_compressor->Buf(rawBuf.data(), &size);
+                m_compressor->zBuf(&size);
+            }
 
             std::string buf(size + 5u, '\0');
             /// \bug order of evaluation of function arguments is undefined:

tests/CMakeLists.txt
@@ -38,7 +38,6 @@ SET(test_PROGRAMS
     romantest
     striptest
     swaptest
-    testblocks
     utf8norm
     versekeytest
     vtreekeytest

tests/testblocks.cpp
@@ -1,110 +1,0 @@
-/******************************************************************************
- *
- *  testblocks.cpp -
- *
- * $Id$
- *
- * Copyright 2001-2013 CrossWire Bible Society (http://www.crosswire.org)
- *    CrossWire Bible Society
- *    P. O. Box 2528
- *    Tempe, AZ  85280-2528
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation version 2.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- */
-
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <swordxx/modules/common/entriesblk.h>
-#include <string>
-
-
-using namespace std;
-using namespace swordxx;
-
-void addEntry(EntriesBlock *eb) {
-    string input;
-    string body;
-    char line[1024];
-    std::cout << "\nEnter new Entry's text. '.' on an empty line to finish:\n";
-    do {
-        std::cout << "> ";
-        fgets(line, 1000, stdin);
-        input = line;
-        if (input.compare("."))
-            body.append(input);
-    }
-    while (input.compare("."));
-    std::cout << "Adding new entry.  Index is: " << eb->addEntry(body.c_str()) << "\n\n";
-}
-
-
-void printEntry(EntriesBlock *eb, int index) {
-    if (index < eb->getCount()) {
-        std::cout << "Contents of entry [" << index << "]:\n";
-        std::cout << eb->getEntry(index) << "\n";
-    }
-    else std::cout << "Invalid entry number\n\n";
-}
-
-
-void printSize(EntriesBlock *eb) {
-    unsigned long size;
-    eb->getRawData(&size);
-    std::cout << "Size of raw data: " << size << "\n\n";
-}
-
-
-void removeEntry(EntriesBlock *eb, int index) {
-    if (index < eb->getCount()) {
-        std::cout << "Removing entry [" << index << "]\n";
-        eb->removeEntry(index);
-    }
-    else std::cout << "Invalid entry number\n\n";
-}
-
-
-int main(int /* argc */, char ** /* argv */) {
-
-    EntriesBlock *eb = new EntriesBlock();
-    string input;
-    char line[1024];
-
-    std::cout << "Initial entry count should be 0: " << eb->getCount() << "\n";
-
-    do {
-        std::cout << "[" << eb->getCount() << "] > ";
-        fgets(line, 1000, stdin);
-        input = line;
-        if (input.length() > 0) {
-            switch (input[0]) {
-                case 'a': addEntry(eb); break;
-                case 'p':    printEntry(eb, std::atoi(input.c_str()+1)); break;
-                case 'r':    removeEntry(eb, std::atoi(input.c_str()+1)); break;
-                case 's': printSize(eb); break;
-                case 'q': break;
-                case '?':
-                default:
-                    std::cout << "\n a - add a new entry\n";
-                    std::cout << " p <entry_index> - print entry\n";
-                    std::cout << " r <entry_index> - remove entry\n";
-                    std::cout << " s - print size of raw data\n";
-                    std::cout << " q - quit\n\n";
-                    break;
-            }
-        }
-    }
-    while (input.compare("q"));
-
-    delete eb;
-
-    return 0;
-}

commit d1258014722ad7248ec9d46df933d2ec69ac72f3

modules, EntriesBlock: Multiple fixes to bugs introduced in 5cb8be2c20.

src/swordxx/modules/common/entriesblk.cpp
@@ -53,16 +53,21 @@ inline constexpr bool sumFits(T a, T b, Args && ... args) noexcept {
 
 } // anonymous namespace
 
-EntriesBlock::EntriesBlock(void const * iBlock, std::size_t size) {
+EntriesBlock::EntriesBlock(void const * iBlock, std::size_t size)
+    : m_serializedSize(size ? size : sizeof(SizeType))
+{
+    if (!size)
+        return;
     auto readPtr(static_cast<char const *>(iBlock));
     auto sizeLeft(size);
     auto const read =
-            [&readPtr, &sizeLeft](void const * data, std::size_t dataSize) {
+            [&readPtr, &sizeLeft](void * const data, std::size_t const dataSize)
+            {
                 if (sizeLeft < dataSize)
                     return false;
-                std::memcpy(&data, readPtr, sizeof(dataSize));
-                readPtr += sizeof(dataSize);
-                sizeLeft -= sizeof(dataSize);
+                std::memcpy(data, readPtr, dataSize);
+                readPtr += dataSize;
+                sizeLeft -= dataSize;
                 return true;
             };
     auto const read32 =
@@ -96,8 +101,10 @@ EntriesBlock::EntriesBlock(void const * iBlock, std::size_t size) {
         std::vector<char> entryBuffer;
         for (auto const & metaEntry : metaInfo) {
             // Drop removed entries:
-            if (!metaEntry.offset)
+            if (!metaEntry.offset) {
+                m_serializedSize -= METAENTRYSIZE;
                 continue;
+            }
 
             if (metaEntry.size <= 0u)
                 throw std::runtime_error("Unexpected empty entry!");
@@ -201,8 +208,8 @@ EntriesBlock::SizeType EntriesBlock::addEntry(std::string entry) {
 }
 
 std::string const & EntriesBlock::operator[](SizeType index) const noexcept {
-    assert(index < m_entries.size());
-    return m_entries[index];
+    static std::string const empty;
+    return (index < m_entries.size()) ? m_entries[index] : empty;
 }
 
 } /* namespace swordxx */