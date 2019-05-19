17f8cc8739f811610087d4c92a22d55168d974ab
scribe <scribe@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2019-05-19 02:49:51 +0000

    commit example app which uses native libsword.so for Android
    git-svn-id: https://crosswire.org/svn/sword/trunk@3626 bcd7d363-81e1-0310-97ec-a550e20fc99c

---
+++ bindings/Android/SWORD/app/src/main/java/org/crosswire/android/sword/MainActivity.java
@@ -0,0 +1,24 @@
+package org.crosswire.android.sword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("sword");
+    }
+    SWMgr mgr = new SWMgr();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText("libsword version " + mgr.version());
+    }
+}
