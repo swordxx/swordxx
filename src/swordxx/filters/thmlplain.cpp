b0e6645a8b76b540d15b750ee407dbdceeacf060
Jaak Ristioja <jaak@ristioja.ee>
2019-07-09 03:22:59 +0300

    filters, ThMLPlain: Retain original until finished.

--- src/swordxx/filters/thmlplain.cpp
+++ src/swordxx/filters/thmlplain.cpp
@@ -46,9 +46,8 @@ char ThMLPlain::processText(std::string &text, const SWKey *key, const SWModule 
     bool ampersand = false;
 
     const char *from;
-    std::string orig = text;
-    from = orig.c_str();
-    for (text = ""; *from; from++)
+    std::string out;
+    for (auto const * from = text.c_str(); *from; from++)
     {
         if (*from == 10 || *from == 13)
             from++;
@@ -74,109 +73,109 @@ char ThMLPlain::processText(std::string &text, const SWKey *key, const SWModule 
             intoken = false;
             ampersand = false;
 
-            if (startsWith(token, "nbsp"sv)) text += ' ';
-            else if (startsWith(token, "quot"sv)) text += '"';
-            else if (startsWith(token, "amp"sv)) text += '&';
-            else if (startsWith(token, "lt"sv)) text += '<';
-            else if (startsWith(token, "gt"sv)) text += '>';
-            else if (startsWith(token, "brvbar"sv)) text += "¦";
-            else if (startsWith(token, "sect"sv)) text += "§";
-            else if (startsWith(token, "copy"sv)) text += "©";
-            else if (startsWith(token, "laquo"sv)) text += "«";
-            else if (startsWith(token, "reg"sv)) text += "®";
-            else if (startsWith(token, "acute"sv)) text += "´";
-            else if (startsWith(token, "para"sv)) text += "¶";
-            else if (startsWith(token, "raquo"sv)) text += "»";
+            if (startsWith(token, "nbsp"sv)) out += ' ';
+            else if (startsWith(token, "quot"sv)) out += '"';
+            else if (startsWith(token, "amp"sv)) out += '&';
+            else if (startsWith(token, "lt"sv)) out += '<';
+            else if (startsWith(token, "gt"sv)) out += '>';
+            else if (startsWith(token, "brvbar"sv)) out += "¦";
+            else if (startsWith(token, "sect"sv)) out += "§";
+            else if (startsWith(token, "copy"sv)) out += "©";
+            else if (startsWith(token, "laquo"sv)) out += "«";
+            else if (startsWith(token, "reg"sv)) out += "®";
+            else if (startsWith(token, "acute"sv)) out += "´";
+            else if (startsWith(token, "para"sv)) out += "¶";
+            else if (startsWith(token, "raquo"sv)) out += "»";
 
-            else if (startsWith(token, "Aacute"sv)) text += "Á";
-            else if (startsWith(token, "Agrave"sv)) text += "À";
-            else if (startsWith(token, "Acirc"sv)) text += "Â";
-            else if (startsWith(token, "Auml"sv)) text += "Ä";
-            else if (startsWith(token, "Atilde"sv)) text += "Ã";
-            else if (startsWith(token, "Aring"sv)) text += "Å";
-            else if (startsWith(token, "aacute"sv)) text += "á";
-            else if (startsWith(token, "agrave"sv)) text += "à";
-            else if (startsWith(token, "acirc"sv)) text += "â";
-            else if (startsWith(token, "auml"sv)) text += "ä";
-            else if (startsWith(token, "atilde"sv)) text += "ã";
-            else if (startsWith(token, "aring"sv)) text += "å";
-            else if (startsWith(token, "Eacute"sv)) text += "É";
-            else if (startsWith(token, "Egrave"sv)) text += "È";
-            else if (startsWith(token, "Ecirc"sv)) text += "Ê";
-            else if (startsWith(token, "Euml"sv)) text += "Ë";
-            else if (startsWith(token, "eacute"sv)) text += "é";
-            else if (startsWith(token, "egrave"sv)) text += "è";
-            else if (startsWith(token, "ecirc"sv)) text += "ê";
-            else if (startsWith(token, "euml"sv)) text += "ë";
-            else if (startsWith(token, "Iacute"sv)) text += "Í";
-            else if (startsWith(token, "Igrave"sv)) text += "Ì";
-            else if (startsWith(token, "Icirc"sv)) text += "Î";
-            else if (startsWith(token, "Iuml"sv)) text += "Ï";
-            else if (startsWith(token, "iacute"sv)) text += "í";
-            else if (startsWith(token, "igrave"sv)) text += "ì";
-            else if (startsWith(token, "icirc"sv)) text += "î";
-            else if (startsWith(token, "iuml"sv)) text += "ï";
-            else if (startsWith(token, "Oacute"sv)) text += "Ó";
-            else if (startsWith(token, "Ograve"sv)) text += "Ò";
-            else if (startsWith(token, "Ocirc"sv)) text += "Ô";
-            else if (startsWith(token, "Ouml"sv)) text += "Ö";
-            else if (startsWith(token, "Otilde"sv)) text += "Õ";
-            else if (startsWith(token, "oacute"sv)) text += "ó";
-            else if (startsWith(token, "ograve"sv)) text += "ò";
-            else if (startsWith(token, "ocirc"sv)) text += "ô";
-            else if (startsWith(token, "ouml"sv)) text += "ö";
-            else if (startsWith(token, "otilde"sv)) text += "õ";
-            else if (startsWith(token, "Uacute"sv)) text += "Ú";
-            else if (startsWith(token, "Ugrave"sv)) text += "Ù";
-            else if (startsWith(token, "Ucirc"sv)) text += "Û";
-            else if (startsWith(token, "Uuml"sv)) text += "Ü";
-            else if (startsWith(token, "uacute"sv)) text += "ú";
-            else if (startsWith(token, "ugrave"sv)) text += "ù";
-            else if (startsWith(token, "ucirc"sv)) text += "û";
-            else if (startsWith(token, "uuml"sv)) text += "ü";
-            else if (startsWith(token, "Yacute"sv)) text += "Ý";
-            else if (startsWith(token, "yacute"sv)) text += "ý";
-            else if (startsWith(token, "yuml"sv)) text += "ÿ";
+            else if (startsWith(token, "Aacute"sv)) out += "Á";
+            else if (startsWith(token, "Agrave"sv)) out += "À";
+            else if (startsWith(token, "Acirc"sv)) out += "Â";
+            else if (startsWith(token, "Auml"sv)) out += "Ä";
+            else if (startsWith(token, "Atilde"sv)) out += "Ã";
+            else if (startsWith(token, "Aring"sv)) out += "Å";
+            else if (startsWith(token, "aacute"sv)) out += "á";
+            else if (startsWith(token, "agrave"sv)) out += "à";
+            else if (startsWith(token, "acirc"sv)) out += "â";
+            else if (startsWith(token, "auml"sv)) out += "ä";
+            else if (startsWith(token, "atilde"sv)) out += "ã";
+            else if (startsWith(token, "aring"sv)) out += "å";
+            else if (startsWith(token, "Eacute"sv)) out += "É";
+            else if (startsWith(token, "Egrave"sv)) out += "È";
+            else if (startsWith(token, "Ecirc"sv)) out += "Ê";
+            else if (startsWith(token, "Euml"sv)) out += "Ë";
+            else if (startsWith(token, "eacute"sv)) out += "é";
+            else if (startsWith(token, "egrave"sv)) out += "è";
+            else if (startsWith(token, "ecirc"sv)) out += "ê";
+            else if (startsWith(token, "euml"sv)) out += "ë";
+            else if (startsWith(token, "Iacute"sv)) out += "Í";
+            else if (startsWith(token, "Igrave"sv)) out += "Ì";
+            else if (startsWith(token, "Icirc"sv)) out += "Î";
+            else if (startsWith(token, "Iuml"sv)) out += "Ï";
+            else if (startsWith(token, "iacute"sv)) out += "í";
+            else if (startsWith(token, "igrave"sv)) out += "ì";
+            else if (startsWith(token, "icirc"sv)) out += "î";
+            else if (startsWith(token, "iuml"sv)) out += "ï";
+            else if (startsWith(token, "Oacute"sv)) out += "Ó";
+            else if (startsWith(token, "Ograve"sv)) out += "Ò";
+            else if (startsWith(token, "Ocirc"sv)) out += "Ô";
+            else if (startsWith(token, "Ouml"sv)) out += "Ö";
+            else if (startsWith(token, "Otilde"sv)) out += "Õ";
+            else if (startsWith(token, "oacute"sv)) out += "ó";
+            else if (startsWith(token, "ograve"sv)) out += "ò";
+            else if (startsWith(token, "ocirc"sv)) out += "ô";
+            else if (startsWith(token, "ouml"sv)) out += "ö";
+            else if (startsWith(token, "otilde"sv)) out += "õ";
+            else if (startsWith(token, "Uacute"sv)) out += "Ú";
+            else if (startsWith(token, "Ugrave"sv)) out += "Ù";
+            else if (startsWith(token, "Ucirc"sv)) out += "Û";
+            else if (startsWith(token, "Uuml"sv)) out += "Ü";
+            else if (startsWith(token, "uacute"sv)) out += "ú";
+            else if (startsWith(token, "ugrave"sv)) out += "ù";
+            else if (startsWith(token, "ucirc"sv)) out += "û";
+            else if (startsWith(token, "uuml"sv)) out += "ü";
+            else if (startsWith(token, "Yacute"sv)) out += "Ý";
+            else if (startsWith(token, "yacute"sv)) out += "ý";
+            else if (startsWith(token, "yuml"sv)) out += "ÿ";
 
-            else if (startsWith(token, "deg"sv)) text += "°";
-            else if (startsWith(token, "plusmn"sv)) text += "±";
-            else if (startsWith(token, "sup2"sv)) text += "²";
-            else if (startsWith(token, "sup3"sv)) text += "³";
-            else if (startsWith(token, "sup1"sv)) text += "¹";
-            else if (startsWith(token, "nbsp"sv)) text += "º";
-            else if (startsWith(token, "pound"sv)) text += "£";
-            else if (startsWith(token, "cent"sv)) text += "¢";
-            else if (startsWith(token, "frac14"sv)) text += "¼";
-            else if (startsWith(token, "frac12"sv)) text += "½";
-            else if (startsWith(token, "frac34"sv)) text += "¾";
-            else if (startsWith(token, "iquest"sv)) text += "¿";
-            else if (startsWith(token, "iexcl"sv)) text += "¡";
-            else if (startsWith(token, "ETH"sv)) text += "Ð";
-            else if (startsWith(token, "eth"sv)) text += "ð";
-            else if (startsWith(token, "THORN"sv)) text += "Þ";
-            else if (startsWith(token, "thorn"sv)) text += "þ";
-            else if (startsWith(token, "AElig"sv)) text += "Æ";
-            else if (startsWith(token, "aelig"sv)) text += "æ";
-            else if (startsWith(token, "Oslash"sv)) text += "Ø";
-            else if (startsWith(token, "curren"sv)) text += "¤";
-            else if (startsWith(token, "Ccedil"sv)) text += "Ç";
-            else if (startsWith(token, "ccedil"sv)) text += "ç";
-            else if (startsWith(token, "szlig"sv)) text += "ß";
-            else if (startsWith(token, "Ntilde"sv)) text += "Ñ";
-            else if (startsWith(token, "ntilde"sv)) text += "ñ";
-            else if (startsWith(token, "yen"sv)) text += "¥";
-            else if (startsWith(token, "not"sv)) text += "¬";
-            else if (startsWith(token, "ordf"sv)) text += "ª";
-            else if (startsWith(token, "uml"sv)) text += "¨";
-            else if (startsWith(token, "shy"sv)) text += "­";
-            else if (startsWith(token, "macr"sv)) text += "¯";
-            else if (startsWith(token, "micro"sv)) text += "µ";
-            else if (startsWith(token, "middot"sv)) text += "·";
-            else if (startsWith(token, "cedil"sv)) text += "¸";
-            else if (startsWith(token, "ordm"sv)) text += "º";
-            else if (startsWith(token, "times"sv)) text += "×";
-            else if (startsWith(token, "divide"sv)) text += "÷";
-            else if (startsWith(token, "oslash"sv)) text += "ø";
+            else if (startsWith(token, "deg"sv)) out += "°";
+            else if (startsWith(token, "plusmn"sv)) out += "±";
+            else if (startsWith(token, "sup2"sv)) out += "²";
+            else if (startsWith(token, "sup3"sv)) out += "³";
+            else if (startsWith(token, "sup1"sv)) out += "¹";
+            else if (startsWith(token, "nbsp"sv)) out += "º";
+            else if (startsWith(token, "pound"sv)) out += "£";
+            else if (startsWith(token, "cent"sv)) out += "¢";
+            else if (startsWith(token, "frac14"sv)) out += "¼";
+            else if (startsWith(token, "frac12"sv)) out += "½";
+            else if (startsWith(token, "frac34"sv)) out += "¾";
+            else if (startsWith(token, "iquest"sv)) out += "¿";
+            else if (startsWith(token, "iexcl"sv)) out += "¡";
+            else if (startsWith(token, "ETH"sv)) out += "Ð";
+            else if (startsWith(token, "eth"sv)) out += "ð";
+            else if (startsWith(token, "THORN"sv)) out += "Þ";
+            else if (startsWith(token, "thorn"sv)) out += "þ";
+            else if (startsWith(token, "AElig"sv)) out += "Æ";
+            else if (startsWith(token, "aelig"sv)) out += "æ";
+            else if (startsWith(token, "Oslash"sv)) out += "Ø";
+            else if (startsWith(token, "curren"sv)) out += "¤";
+            else if (startsWith(token, "Ccedil"sv)) out += "Ç";
+            else if (startsWith(token, "ccedil"sv)) out += "ç";
+            else if (startsWith(token, "szlig"sv)) out += "ß";
+            else if (startsWith(token, "Ntilde"sv)) out += "Ñ";
+            else if (startsWith(token, "ntilde"sv)) out += "ñ";
+            else if (startsWith(token, "yen"sv)) out += "¥";
+            else if (startsWith(token, "not"sv)) out += "¬";
+            else if (startsWith(token, "ordf"sv)) out += "ª";
+            else if (startsWith(token, "uml"sv)) out += "¨";
+            else if (startsWith(token, "shy"sv)) out += "­";
+            else if (startsWith(token, "macr"sv)) out += "¯";
+            else if (startsWith(token, "micro"sv)) out += "µ";
+            else if (startsWith(token, "middot"sv)) out += "·";
+            else if (startsWith(token, "cedil"sv)) out += "¸";
+            else if (startsWith(token, "ordm"sv)) out += "º";
+            else if (startsWith(token, "times"sv)) out += "×";
+            else if (startsWith(token, "divide"sv)) out += "÷";
+            else if (startsWith(token, "oslash"sv)) out += "ø";
             continue;
 
         }
@@ -184,32 +183,32 @@ char ThMLPlain::processText(std::string &text, const SWKey *key, const SWModule 
             intoken = false;
             // process desired tokens
             if (startsWith(token, "sync type=\"Strongs\" value=\""sv)) {
-                text += ' ';
-                text += '<';
+                out += ' ';
+                out += '<';
                 for (unsigned int i = 27; token[i] != '\"'; i++)
-                    text += token[i];
-                text += '>';
+                    out += token[i];
+                out += '>';
                 continue;
             }
             if (startsWith(token, "sync type=\"morph\" value=\""sv)) {
-                text += ' ';
-                text += '(';
+                out += ' ';
+                out += '(';
                 for (unsigned int i = 25; token[i] != '\"'; i++)
-                    text += token[i];
-                text += ')';
+                    out += token[i];
+                out += ')';
                 continue;
             }
             if (startsWith(token, "note"sv)) {
-                text += ' ';
-                text += '[';
+                out += ' ';
+                out += '[';
             }
             else if (startsWith(token, "br"sv))
-                text += '\n';
+                out += '\n';
             else if (startsWith(token, "/p"sv))
-                text += '\n';
+                out += '\n';
             else if (startsWith(token, "/note"sv)) {
-                text += ']';
-                text += ' ';
+                out += ']';
+                out += ' ';
             }
             continue;
         }
@@ -224,23 +223,20 @@ char ThMLPlain::processText(std::string &text, const SWKey *key, const SWModule 
                 token[tokpos+2] = 0;
             }
         }
-        else	text += *from;
+        else	out += *from;
     }
 
-    orig = text;
-    from = orig.c_str();
-    for (text = ""; *from; from++) {  //loop to remove extra spaces
-                if ((std::strchr(" \t\n\r", *from))) {
-                        while (*(from+1) && (std::strchr(" \t\n\r", *(from+1)))) {
-                                from++;
-                        }
-                        text += " ";
-                }
-                else {
-                        text += *from;
-                }
+    std::string out2;
+    for (auto const * from = out.c_str(); *from; from++) {  //loop to remove extra spaces
+        if ((std::strchr(" \t\n\r", *from))) {
+            while (*(from+1) && (std::strchr(" \t\n\r", *(from+1))))
+                from++;
+            out2 += " ";
+        } else {
+            out2 += *from;
         }
-
+    }
+    text = std::move(out2);
     return 0;
 }
 
