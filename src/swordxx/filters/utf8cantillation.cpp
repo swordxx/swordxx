53d31d49efaecfd277532905469c0fb8b3d60d3b
Jaak Ristioja <jaak@ristioja.ee>
2019-07-09 03:33:13 +0300

    filters, UTF8Cantillation: Retain original until finished.

--- src/swordxx/filters/utf8cantillation.cpp
+++ src/swordxx/filters/utf8cantillation.cpp
@@ -48,26 +48,26 @@ char UTF8Cantillation::processText(std::string &text, const SWKey *key, const SW
     (void) module;
     if (!option) {
         //The UTF-8 range 0xD6 0x90 to 0xD6 0xAF and 0xD7 0x84 consist of Hebrew cantillation marks so block those out.
-        std::string orig = text;
-        const unsigned char* from = (unsigned char*)orig.c_str();
-        for (text = ""; *from; from++) {
+        std::string out;
+        for (auto from = reinterpret_cast<unsigned char const *>(text.c_str()); *from; from++) {
             if (*from != 0xD6) {
                 if (*from == 0xD7 && *(from + 1) == 0x84) {
                     from++;
                 }
                 else {
-                    text += *from;
+                    out += *from;
                 }
             }
             else if (*(from + 1) < 0x90 || *(from + 1) > 0xAF) {
-                text += *from;
+                out += *from;
                 from++;
-                text += *from;
+                out += *from;
             }
             else {
                 from++;
             }
         }
+        text = std::move(out);
     }
     return 0;
 }
