28b895509b9a0d5654374b80958b3fdd7d52f898
Jaak Ristioja <jaak@ristioja.ee>
2019-09-15 01:13:46 +0300

    travis-ci/script.sh: Improved error safety and verbosity

--- travis-ci/script.sh
+++ travis-ci/script.sh
@@ -4,6 +4,9 @@
 # tests. It is not meant to be demonstrative, used by end users, or
 # any other set of such consumption. Although you can choose to take
 # it as informative if you wish
+
+set -evuo pipefail
+
 mkdir -p build
 cd build
 /opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=install/ -DSWORDXX_BUILD_EXAMPLES=ON -DSWORDXX_BUILD_TESTS=ON .. || exit 1
