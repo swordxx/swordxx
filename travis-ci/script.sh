8e556ad31eeb2a86c01c4f4ca84894c3a060976a
Jaak Ristioja <jaak@ristioja.ee>
2019-09-15 01:20:09 +0300

    travis-ci/script.sh: Removed redundant || exit 1 conditions

--- travis-ci/script.sh
+++ travis-ci/script.sh
@@ -9,7 +9,7 @@ set -evuo pipefail
 
 mkdir -p build
 cd build
-/opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=install/ -DSWORDXX_BUILD_EXAMPLES=ON -DSWORDXX_BUILD_TESTS=ON .. || exit 1
-make VERBOSE=1 || exit 1
-make VERBOSE=1 tests || exit 1
-make VERBOSE=1 install || exit 1
+/opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=install/ -DSWORDXX_BUILD_EXAMPLES=ON -DSWORDXX_BUILD_TESTS=ON ..
+make VERBOSE=1
+make VERBOSE=1 tests
+make VERBOSE=1 install
