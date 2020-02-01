0c4d192bb93937c978ae57c2d0a35b68dd665d31
Jaak Ristioja <jaak@ristioja.ee>
2020-02-02 01:46:15 +0200

    filters, UTF8SCSU: Rewrote based on UnicodeConversionFilter

--- src/swordxx/filters/utf8scsu.cpp
+++ src/swordxx/filters/utf8scsu.cpp
@@ -1,13 +1,6 @@
 /******************************************************************************
  *
- *  utf8scsu.cpp -    SWFilter descendant to convert UTF-8 to SCSU
- *
- * $Id$
- *
- * Copyright 2001-2014 CrossWire Bible Society (http://www.crosswire.org)
- *    CrossWire Bible Society
- *    P. O. Box 2528
- *    Tempe, AZ  85280-2528
+ * Copyright Jaak Ristioja
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -22,39 +15,5 @@
 
 #include "utf8scsu.h"
 
-#include <string>
 
-namespace swordxx {
-
-
-UTF8SCSU::UTF8SCSU() {
-    // initialize SCSU converter
-    scsuConv = ucnv_open("SCSU", &err);
-
-    // initialize UTF-8 converter
-    utf8Conv = ucnv_open("UTF-8", &err);
-}
-
-UTF8SCSU::~UTF8SCSU() {
-         ucnv_close(scsuConv);
-         ucnv_close(utf8Conv);
-}
-
-char UTF8SCSU::processText(std::string &text, const SWKey *key, const SWModule *module) {
-    (void) module;
-    if ((unsigned long)key < 2)    // hack, we're en(1)/de(0)ciphering
-        return -1;
-
-    err = U_ZERO_ERROR;
-    icu::UnicodeString utf16Text(text.c_str(), text.length(), utf8Conv, err);
-    err = U_ZERO_ERROR;
-    int32_t len = utf16Text.extract(&text[0u], text.size(), scsuConv, err);
-    if (len > (int32_t)text.size()+1) {
-        text.resize(len + 1, '\0');
-        utf16Text.extract(&text[0u], text.size(), scsuConv, err);
-    }
-
-    return 0;
-}
-
-} /* namespace swordxx */
+SWORDXX_DEFINE_UNICODE_CONVERSION_FILTER(UTF8SCSU, "SCSU", encode)
--- src/swordxx/filters/utf8scsu.h
+++ src/swordxx/filters/utf8scsu.h
@@ -1,13 +1,6 @@
 /******************************************************************************
  *
- *  utf8scsu.h -    Implementation of UTF8SCSU
- *
- * $Id$
- *
- * Copyright 2001-2014 CrossWire Bible Society (http://www.crosswire.org)
- *    CrossWire Bible Society
- *    P. O. Box 2528
- *    Tempe, AZ  85280-2528
+ * Copyright Jaak Ristioja
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -23,31 +16,9 @@
 #ifndef SWORDXX_UTF8SCSU_H
 #define SWORDXX_UTF8SCSU_H
 
-#include "../swfilter.h"
-
-#include <unicode/utypes.h>
-#include <unicode/ucnv.h>
-#include <unicode/uchar.h>
-#include <unicode/unistr.h>
+#include "../UnicodeConversionFilter.h"
 
 
-namespace swordxx {
-
-/** This filter converts UTF-8 encoded text to SCSU
- */
-class SWDLLEXPORT UTF8SCSU : public SWFilter {
-private:
-    UConverter* scsuConv;
-    UConverter* utf8Conv;
-    UErrorCode err;
-public:
-    UTF8SCSU();
-    ~UTF8SCSU() override;
-    char processText(std::string & text,
-                     SWKey const * key = nullptr,
-                     SWModule const * module = nullptr) override;
-};
-
-} /* namespace swordxx */
+SWORDXX_DECLARE_UNICODE_CONVERSION_FILTER(UTF8SCSU)
 
 #endif /* SWORDXX_UTF8SCSU_H */
