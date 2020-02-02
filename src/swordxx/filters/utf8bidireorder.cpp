e765fa512ac529c834c949e8dd9c332a4bb72693
Jaak Ristioja <jaak@ristioja.ee>
2020-02-02 02:36:49 +0200

    filters, UTF8BiDiReorder: Refactored to use utf8ToUtf16() and utf16ToUtf8()

--- src/swordxx/filters/utf8bidireorder.cpp
+++ src/swordxx/filters/utf8bidireorder.cpp
@@ -31,8 +31,6 @@
 #include <stdexcept>
 #include <string>
 #include <unicode/ubidi.h>
-#include <unicode/uchar.h>
-#include <unicode/ucnv.h>
 #include <unicode/utypes.h>
 #include <vector>
 #include "../DebugOnly.h"
@@ -54,54 +52,8 @@ char UTF8BiDiReorder::processText(std::string & text,
     if ((unsigned long)key < 2)    // hack, we're en(1)/de(0)ciphering
         return -1;
 
-    std::unique_ptr<::UConverter, void (*)(::UConverter *)> conv(
-                []() {
-                    ::UErrorCode err = U_ZERO_ERROR;
-                    return ::ucnv_open("UTF-8", &err);
-                }(),
-                +[](::UConverter * const p) noexcept { ::ucnv_close(p); });
-
-    if (text.size() > toUnsigned(max_v<std::int32_t>))
-        throw std::runtime_error("Implementation limits reached!");
-
     // Convert UTF-8 string to UTF-16 (UChars):
-    std::vector<::UChar> ustr;
-    {
-        ::UErrorCode err = U_ZERO_ERROR;
-        auto const sourceLen = std::strlen(text.c_str());
-        if (sourceLen >= toUnsigned(max_v<std::int32_t>))
-            throw std::runtime_error("Implementation limits reached!");
-        auto const len = ::ucnv_toUChars(conv.get(),
-                                         nullptr,
-                                         0,
-                                         text.c_str(),
-                                         static_cast<std::int32_t>(sourceLen),
-                                         &err);
-        if (U_FAILURE(err))
-            throw std::runtime_error("::ucnv_toUChars() failed!");
-        assert(len >= 0);
-        if (len >= max_v<decltype(len)>) // need to increment later
-            throw std::runtime_error("Implementation limits reached!");
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wtype-limits"
-        if constexpr (toUnsigned(max_v<decltype(len)>)
-                      > max_v<decltype(ustr)::size_type>)
-            if (toUnsigned(len) > max_v<decltype(ustr)::size_type>)
-                throw std::runtime_error("Implementation limits reached!");
-        #pragma GCC diagnostic pop
-        ustr.resize(static_cast<decltype(ustr)::size_type>(toUnsigned(len)));
-        err = U_ZERO_ERROR;
-        SWORDXX_DEBUG_ONLY(auto const r =)
-                ::ucnv_toUChars(conv.get(),
-                                ustr.data(),
-                                len,
-                                text.c_str(),
-                                static_cast<std::int32_t>(sourceLen),
-                                &err);
-        assert(r == len);
-        if (U_FAILURE(err))
-            throw std::runtime_error("::ucnv_toUChars() failed!");
-    }
+    auto ustr(utf8ToUtf16(text));
 
     decltype(ustr) ustr2;
     assert(ustr.size() < toUnsigned(max_v<std::int32_t>));
@@ -113,7 +65,7 @@ char UTF8BiDiReorder::processText(std::string & text,
                         return ::ubidi_openSized(len + 1, 0, &err);
                     }(),
                     +[](::UBiDi * const p) noexcept { ::ubidi_close(p); });
-        if (!conv)
+        if (!bidi)
             throw std::runtime_error("::ubidi_openSized() failed!");
         {
             ::UErrorCode err = U_ZERO_ERROR;
@@ -158,38 +110,7 @@ char UTF8BiDiReorder::processText(std::string & text,
 //                UBIDI_DO_MIRRORING | UBIDI_REMOVE_BIDI_CONTROLS, &err);
 #endif
 
-    UErrorCode err = U_ZERO_ERROR;
-    auto const len =
-            ::ucnv_fromUChars(conv.get(),
-                              nullptr,
-                              0,
-                              ustr2.data(),
-                              static_cast<std::int32_t>(ustr2.size()),
-                              &err);
-    if (U_FAILURE(err))
-        throw std::runtime_error("::ucnv_fromUChars() failed!");
-    assert(len >= 0);
-    std::string out;
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wtype-limits"
-    if constexpr (toUnsigned(max_v<decltype(len)>)
-                  > max_v<decltype(out)::size_type>)
-        if (toUnsigned(len) > max_v<decltype(out)::size_type>)
-            throw std::runtime_error("Implementation limits reached!");
-    #pragma GCC diagnostic pop
-    out.resize(static_cast<decltype(out)::size_type>(toUnsigned(len)));
-    err = U_ZERO_ERROR;
-    SWORDXX_DEBUG_ONLY(auto const r =)
-            ::ucnv_fromUChars(conv.get(),
-                              out.data(),
-                              len,
-                              ustr2.data(),
-                              static_cast<std::int32_t>(ustr2.size()),
-                              &err);
-    assert(r == len);
-    if (U_FAILURE(err))
-        throw std::runtime_error("::ucnv_fromUChars() failed!");
-    text.swap(out);
+    text = utf16ToUtf8(ustr2);
     return 0;
 }
 
