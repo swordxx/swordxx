80ab16add9544b83ba1a091db497a17a1c1dea92
refdoc <refdoc@bcd7d363-81e1-0310-97ec-a550e20fc99c>
2020-02-02 20:59:36 +0000

    Added tags <list> and <item>
    git-svn-id: https://crosswire.org/svn/sword/trunk@3692 bcd7d363-81e1-0310-97ec-a550e20fc99c

--- src/modules/filters/teihtmlhref.cpp
+++ src/modules/filters/teihtmlhref.cpp
@@ -322,6 +322,48 @@ bool TEIHTMLHREF::handleToken(SWBuf &buf, const char *token, BasicFilterUserData
 			}
 		}
 
+		// <list> <item>
+		else if (!strcmp(tag.getName(), "list")) {
+			if ((!tag.isEndTag()) && (!tag.isEmpty())) {
+
+				SWBuf rend = tag.getAttribute("rend");
+				
+				u->lastHi = rend;
+				if (rend == "numbered") {
+					buf += "<ol>\n";
+					}
+				else if (rend == "bulletted") {
+					buf += "<ul>\n";
+				}
+				else {
+					buf += "<span class=\"list "; 
+					buf += rend.c_str(); 
+					buf += "\">";
+				}
+			}
+			else if (tag.isEndTag()) {
+				SWBuf rend = u->lastHi;
+				if (rend == "numbered") {
+					buf += "</ol>\n>";
+				}
+				else if (rend == "bulleted") {
+					buf += "</ul>\n";
+				
+				}
+				else {
+					buf += "</span>\n";
+				}
+				u->supressAdjacentWhitespace = true;
+			}
+		}
+		else if (!strcmp(tag.getName(), "item")) {
+			if ((!tag.isEndTag()) && (!tag.isEmpty())) {
+				buf += "<li>";
+			}
+			else if (tag.isEndTag()) {
+				buf += "</li>\n";
+			}
+		}
 		else {
 			return false;  // we still didn't handle token
 		}
